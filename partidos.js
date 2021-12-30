2a2b3d4cb80fab60c2e70a7a1577657fd548bf47
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2021-12-29 23:03:17 -0500

    Update partidos.js

--- partidos.js
+++ partidos.js
@@ -1,3 +1,17 @@
 let PARTIDOS = [
-    [10500,"CHI1","Beijing Guoan","Shandong Taishan",8],[10500,"CHI1","Hebei","Guangzhou FC",8],[10700,"CHI1","Guangzhou City","Shanghai Port",8],[10700,"CHI1","Shenzhen","Changchun Yatai",7],[11445,"ESC2","Dunfermline","Inverness",8],[11445,"ESC2","Kilmarnock","Morton",7],[11445,"ESC2","Partick Thistle","Ayr",10],[11430,"ING1","Chelsea","Brighton",1],[11515,"ING1","Brentford","Manchester City",2],[11445,"ING2","Blackburn","Barnsley",7],[11445,"ING2","Blackpool","Middlesbrough",8],[11445,"ING2","Coventry","Millwall",10],[11445,"INGO","Ipswich","Wycombe",10],[11445,"INGO","Morecambe","Crewe",10],[11445,"INGO","Oxford Utd","Wimbledon",10],[11445,"INGO","Shrewsbury","Accrington",7],[11445,"INGT","Barrow","Oldham",7],[11445,"INGT","Swindon","Stevenage",7],[11200,"POR1","Famalicao","Belenenses",7],[11400,"POR1","V. Guimaraes","Boavista",7],[11600,"POR1","Sporting CP","Portimonense",1],[11030,"POR2","Academico Viseu","Nacional",8],[10500,"TURC","Alanyaspor","Mardin BB",7],[10700,"TURC","Karagumruk","Bodrumspor",7],[10900,"TURC","Adana Demirspor","Ankaraspor",7],[11030,"TURC","Göztepe","Samsunspor",10],[11300,"TURC","Fenerbahçe","Afjet Afyonspor",7]
+    [11515, 'ING1', 'Manchester Utd', 'Burnley'],
+    [11445, 'ING2', 'Bournemouth', 'Cardiff'],
+    [11445, 'ING2', 'Bristol City', 'QPR'],
+    [11445, 'ING2', 'Nottingham Forest', 'Huddersfield'],
+    [11445, 'ING2', 'Stoke', 'Derby'],
+    [11445, 'INGO', 'Sunderland', 'Sheffield Wed'],
+    [11200, 'POR1', 'P. Ferreira', 'Santa Clara'],
+    [11400, 'POR1', 'Arouca', 'SC Braga'],
+    [11600, 'POR1', 'Oporto', 'Benfica'],
+    [10500, 'TURC', 'Yeni Malatyaspor', 'Bandirmaspor'],
+    [10700, 'TURC', 'Kayserispor', 'Aksaray Bld.'],
+    [10700, 'TURC', 'Konyaspor', 'Umraniyespor'],
+    [10900, 'TURC', 'Sivasspor', 'Ankaragucu'],
+    [11030, 'TURC', 'Gaziantep', 'Bursaspor'],
+    [11300, 'TURC', 'Beşiktaş', 'Altay'],
 ]
