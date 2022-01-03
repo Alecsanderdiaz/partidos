71399cae6340d0eadcefb73a07986666ca67d065
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2022-01-03 11:25:01 -0500

    Update partidos.js

--- partidos.js
+++ partidos.js
@@ -1,17 +1,3 @@
 let PARTIDOS = [
-    [11515, 'ING1', 'Manchester Utd', 'Burnley'],
-    [11445, 'ING2', 'Bournemouth', 'Cardiff'],
-    [11445, 'ING2', 'Bristol City', 'QPR'],
-    [11445, 'ING2', 'Nottingham Forest', 'Huddersfield'],
-    [11445, 'ING2', 'Stoke', 'Derby'],
-    [11445, 'INGO', 'Sunderland', 'Sheffield Wed'],
-    [11200, 'POR1', 'P. Ferreira', 'Santa Clara'],
-    [11400, 'POR1', 'Arouca', 'SC Braga'],
-    [11600, 'POR1', 'Oporto', 'Benfica'],
-    [10500, 'TURC', 'Yeni Malatyaspor', 'Bandirmaspor'],
-    [10700, 'TURC', 'Kayserispor', 'Aksaray Bld.'],
-    [10700, 'TURC', 'Konyaspor', 'Umraniyespor'],
-    [10900, 'TURC', 'Sivasspor', 'Ankaragucu'],
-    [11030, 'TURC', 'Gaziantep', 'Bursaspor'],
-    [11300, 'TURC', 'Beşiktaş', 'Altay'],
+    [11230,"ING1","Manchester Utd","Wolves",1],[11300,"ESP1","Villarreal","Levante",1],[11300,"POR2","Vilafranquense","Casa Pia",8],[11500,"ESP1","Osasuna","Athletic Club",8],[11500,"ESP2","Alcorcón","Málaga",10],[11510,"FRAC","Vannes","PSG",2],[11515,"ESP1","Cádiz","Sevilla",2],[11515,"POR1","Tondela","Moreirense",7]
 ]
