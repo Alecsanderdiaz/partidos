c3a0ff0c82cf93a1698a2dbdf59ea4175f8e36f6
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2022-02-09 08:58:18 -0500

    Update logica.js

--- logica.js
+++ logica.js
@@ -9,16 +9,18 @@ const PARTIDOS_COMPLETOS = JSON.parse(JSON.stringify(PARTIDOS))
 const renderizar = () => {
     let contenidoHtml = '';
     let contenidoGrupos = (index) => `
-        <span class="badge badge-primary pointer" onClick="escogerGrupo(1, ${index})">1</span>
-        <span class="badge badge-primary pointer" onClick="escogerGrupo(2, ${index})">2</span>
-        <span class="badge badge-success pointer" onClick="escogerGrupo(3, ${index})">3</span>
-        <span class="badge badge-success pointer" onClick="escogerGrupo(4, ${index})">4</span>
-        <span class="badge badge-secondary pointer" onClick="escogerGrupo(5, ${index})">5</span>
-        <span class="badge badge-secondary pointer" onClick="escogerGrupo(6, ${index})">6</span>
-        <span class="badge badge-warning pointer" onClick="escogerGrupo(7, ${index})">7</span>
-        <span class="badge badge-warning pointer" onClick="escogerGrupo(8, ${index})">8</span>
-        <span class="badge badge-danger pointer" onClick="escogerGrupo(9, ${index})">9</span>
-        <span class="badge badge-danger pointer" onClick="escogerGrupo(10, ${index})">10</span>
+        <span class="ml-3 badge badge-primary pointer" onClick="escogerGrupo(1, ${index})">1</span>
+        <span class="ml-3 badge badge-primary pointer" onClick="escogerGrupo(2, ${index})">2</span>
+        <span class="ml-3 badge badge-success pointer" onClick="escogerGrupo(3, ${index})">3</span>
+        <span class="ml-3 badge badge-success pointer" onClick="escogerGrupo(4, ${index})">4</span>
+        <span class="ml-3 badge badge-secondary pointer" onClick="escogerGrupo(5, ${index})">5</span>
+        <span class="ml-3 badge badge-secondary pointer" onClick="escogerGrupo(6, ${index})">6</span>
+        <span class="ml-3 badge badge-warning pointer" onClick="escogerGrupo(7, ${index})">7</span>
+        <span class="ml-3 badge badge-warning pointer" onClick="escogerGrupo(8, ${index})">8</span>
+        <span class="ml-3 badge badge-danger pointer" onClick="escogerGrupo(9, ${index})">9</span>
+        <span class="ml-3 badge badge-danger pointer" onClick="escogerGrupo(10, ${index})">10</span>
+        <span class="ml-3 badge badge-dark pointer" onClick="escogerGrupo(11, ${index})">11</span>
+        <span class="ml-3 badge badge-dark pointer" onClick="escogerGrupo(12, ${index})">12</span>
     `;
     PARTIDOS.forEach((partido, index) => {
         contenidoHtml += `
@@ -31,8 +33,8 @@ const renderizar = () => {
             <strong>${ partido[5] || 0 }</strong>
             </div>
             <div class="col">
-               ${ partido[4] === 1 ? `<span class="badge badge-primary">${ partido[2] }</span>`: `${ partido[4] === 3 ? `<span class="badge badge-success">${ partido[2] }</span>`: `${ partido[4] === 5 ? `<span class="badge badge-secondary">${ partido[2] }</span>`: `${ partido[4] === 7 ? `<span class="badge badge-warning">${ partido[2] }</span>`: `${ partido[4] === 9 ? `<span class="badge badge-danger">${ partido[2] }</span>`: `${ partido[2]}`}`}`}`}`}<br>
-               ${ partido[4] === 2 ? `<span class="badge badge-primary">${ partido[3] }</span>`: `${ partido[4] === 4 ? `<span class="badge badge-success">${ partido[3] }</span>`: `${ partido[4] === 6 ? `<span class="badge badge-secondary">${ partido[3] }</span>`: `${ partido[4] === 8 ? `<span class="badge badge-warning">${ partido[3] }</span>`: `${ partido[4] === 10 ? `<span class="badge badge-danger">${ partido[3] }</span>`: `${ partido[3]}`}`}`}`}`}
+               ${ partido[4] === 1 ? `<span class="badge badge-primary">${ partido[2] }</span>`: `${ partido[4] === 3 ? `<span class="badge badge-success">${ partido[2] }</span>`: `${ partido[4] === 5 ? `<span class="badge badge-secondary">${ partido[2] }</span>`: `${ partido[4] === 7 ? `<span class="badge badge-warning">${ partido[2] }</span>`: `${ partido[4] === 9 ? `<span class="badge badge-danger">${ partido[2] }</span>`: `${ partido[4] === 11 ? `<span class="badge badge-danger">${ partido[2][0] }</span>${ partido[2].substring(1) }`: `${ partido[2]}`}`}`}`}`}`}<br>
+               ${ partido[4] === 2 ? `<span class="badge badge-primary">${ partido[3] }</span>`: `${ partido[4] === 4 ? `<span class="badge badge-success">${ partido[3] }</span>`: `${ partido[4] === 6 ? `<span class="badge badge-secondary">${ partido[3] }</span>`: `${ partido[4] === 8 ? `<span class="badge badge-warning">${ partido[3] }</span>`: `${ partido[4] === 10 ? `<span class="badge badge-danger">${ partido[3] }</span>`: `${ partido[4] === 11 ? `<span class="badge badge-danger">${ partido[3][0] }</span>${ partido[3].substring(1) }`: `${ partido[3]}`}`}`}`}`}`}
             </div>
             <div class="col d-none d-md-block">
                 <input type="number" value="${ partido[5] ? partido[5] : 0 }" onfocusout="grabarCuota(event, ${index})">
