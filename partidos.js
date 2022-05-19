170dec9f7a9d239ae4999cfb3cdaae1564ce63fc
AlecsanderDiaz <john2005_16@yahoo.com>
2022-05-19 08:00:16 -0500

    jueves

--- partidos.js
+++ partidos.js
@@ -1,3 +1,17 @@
 let PARTIDOS = [
-    [11700,"ZA-LIBE","Palmeiras (Bra)","Emelec (Ecu)",1],[11930,"ZA-SUDA","Santos (Bra)","U. La Calera (Chi)",1],[11700,"ZA-LIBE","Athletico-PR (Bra)","Libertad (Par)",3],[11900,"ZA-LIBE","Tolima (Col)","América-MG (Bra)",3],[11930,"ZA-SUDA","Racing Club (Arg)","Melgar (Per)",3],[11900,"ZA-MEX2","Atlante","Atl. Morelia",5],[11900,"ZA-LIBE","Colón (Arg)","Olimpia (Par)",5],[11930,"ZA-SUDA","LDU Quito (Ecu)","Defensa y Justicia (Arg)",5],[11900,"ZA-EST1","Minnesota United","Los Angeles Galaxy",6],[11930,"ZA-EST1","Houston Dynamo","Seattle Sounders",6],[12100,"ZA-MEX1","Atlas","Tigres UANL",6],[11700,"ZA-LIBE","Nacional (Uru)","Vélez Sarsfield (Arg)",6],[11830,"ZA-EST1","New York Red Bulls","Chicago Fire",7],[12130,"ZA-EST1","Los Angeles FC","Austin FC",7],[12130,"ZA-EST1","San Jose Earthquakes","Portland Timbers",7],[11900,"ZA-LIBE","Dep. Táchira (Ven)","Independiente Petrolero (Bol)",7],[11715,"ZA-SUDA","River Plate (Uru)","Cuiabá (Bra)",7],[11830,"ZA-EST1","DC United","New York City",8],[11930,"ZA-EST1","Sporting Kansas City","Colorado Rapids",8],[12100,"ZA-EST1","Vancouver Whitecaps","FC Dallas",8],[12100,"ZA-SUDA","Alianza Lima (Per)","Fortaleza (Bra)",9],[11930,"ZA-BRA2","CRB","Londrina",10],[11830,"ZA-EST1","Philadelphia Union","Inter Miami",10],[11930,"ZA-EST1","Nashville SC","CF Montreal",10],[11715,"ZA-SUDA","Ayacucho (Per)","Everton (Chi)",10],[11900,"*ZM-COLF","Ind. Medellín F","Deportivo Pereira F",11],[11900,"*ZA-ECU2","Buhos ULVR","Manta",11],[11930,"*ZA-ECU2","Libertad","América de Quito",11]
+    [11700,"ZA-BRA2","Gremio","Criciúma",5],[11700,"ZA-BRA2","Vila Nova","Chapecoense",6],[11700,"ZA-LIBE","Atlético-MG (Bra)","Ind. del Valle (Ecu)",5],[11715,"ZA-SUDA","Independiente (Arg)","La Guaira (Ven)",3],[11715,"ZA-SUDA","Unión Santa Fe (Arg)","Fluminense (Bra)",6],[11715,"*ZA-VEN1","Portuguesa","Hermanos Colmenarez",11],[11900,"*ZA-BOL1","Guabirá","U. Sucre",12],[11900,"*ZA-ECU2","Ind. Juniors","Olmedo",11],[11900,"ZA-LIBE","River Plate (Arg)","Colo Colo (Chi)",5],[11930,"ZA-BRA2","Náutico","CSA",7],[11930,"ZA-BRA2","Guaraní","Vasco",6],[11930,"ZA-SUDA","Lanús (Arg)","Wanderers (Uru)",5],[11930,"ZA-SUDA","Sao Paulo (Bra)","J. Wilstermann (Bol)",3],[11930,"ZA-SUDA","Metropolitanos (Ven)","Barcelona SC (Ecu)",6],[12000,"ZA-MEX1","Club América","Pachuca",5],[12100,"ZA-LIBE","Deportivo Cali (Col)","Always Ready (Bol)",5],
+    [11330,"ALE1","Hertha BSC","Hamburgo",6,2.25,null,2.25],
+[11345,"R-ING1","Everton","Crystal Palace",5,1.95,null,4.38],
+[11400,"R-ING1","Aston Villa","Burnley",5,1.9,null,8.32],
+[11400,"R-ING1","Chelsea","Leicester",1,1.5,null,12.48],
+[11345,"R-INGT","Port Vale","Swindon",10,2.3,null,28.7],
+[11145,"YE-HOL1","Heerenveen","AZ Alkmaar",8,1.85,null,53.09],
+[11400,"YE-HOL1","Utrecht","Vitesse",10,2.3,null,122.1],
+[11000,"YE-REP1","Opava","Bohemians",8,1.95,null,238.09],
+[11000,"YE-REP1","Teplice","Vlasim",10,2.75,null,654.74],
+[11330,"YE-SUI1","Grasshoppers","St. Gallen",7,1.66,null,1086.86],
+[11330,"YE-SUI1","Lausanne","Sion",8,1.75,null,1902],
+[11330,"YE-SUI1","Lucerna","Young Boys",8,1.75,null,3328.5],
+[11330,"YE-SUI1","Lugano","Zúrich",10,1.76,null,5858.16],
+[11330,"YE-SUI1","Servette","Basilea",6,1.76,null,10310.36]
 ]
