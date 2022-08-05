f37febbb688641e00aee1b7388bfe89068a3feb3
AlecsanderDiaz <john2005_16@yahoo.com>
2022-08-05 14:09:17 -0500

    viernes

--- ALGORITMO.js
+++ ALGORITMO.js
@@ -1,33 +1,24 @@
-let PARTIDOS_SIN_CUOTA = [
-  [11101,"*ALEC","Chemnitzer","Union Berlín",12],[11101,"*ALEC","Cottbus","Werder Bremen",12],[11101,"*ALEC","Ingolstadt","Darmstadt",12],[11346,"*ALEC","Magdeburgo","Eintracht Frankfurt",12],[10900,"*AMI","Jong AZ (Ned)","Genk Sub-23 (Bel)",12],[11200,"*AMI","Real Zaragoza (Esp)","Al Nasr Riyadh (Sau)",12],[11400,"*ZA-BOL1","Santa Cruz","Aurora",12],[11615,"*ZA-BOL1","Palmaflor","Nacional Potosí",12],[11830,"*ZA-BOL1","Independiente Petrolero","Oriente Petrolero",12],[11400,"R-ING2","Watford","Sheffield Utd",5],[11200,"YE-POLE","Korona","Slask",8],[11230,"YE-RUM1","FC Hermannstadt","Botosani",7],[11700,"ZA-ARG1","Defensa y Justicia","Arsenal Sarandí",5],[11700,"ZA-ARG1","Rosario Central","Central Córdoba",5],[11930,"ZA-ARG1","Colón","Independiente",7],[11300,"ZA-ARG2","Almagro","Temperley",7],[11300,"ZA-ARG2","Guillermo Brown","Santamarina",7],[11300,"ZA-ARG2","Nueva Chicago","Flandria",7],[11300,"ZA-ARG2","Tristán Suárez","San Telmo",7],[11400,"ZA-ARG2","CA Mitre","Dep. Riestra",7],[11700,"ZA-ARG2","Alvarado","Deportivo Morón",9],[11735,"ZA-ARG2","Ferro Carril Oeste","Belgrano",8],[11900,"ZA-ARG2","San Martín Tuc.","Chaco For Ever",7],[11940,"ZA-ARG2","Instituto","All Boys",10],[11800,"ZA-BRA1","Santos","Fluminense",9],[11800,"ZA-COL1","Atl. Bucaramanga","Tolima",8],[12005,"ZA-COL1","Atl. Nacional","Dep. Pasto",1],[11500,"ZA-COL2","Fortaleza","Bogotá",7],[11600,"ZA-COL2","Quindío","Valledupar",5],[11900,"ZA-ECU1","Nueve de Octubre","Orense",7],[11345,"ZA-FRA2","Sochaux","Paris FC",10],[11300,"ZA-PER1","Sport Boys","César Vallejo",9],[11300,"ZA-URU1","Liverpool M.","Defensor Sp.",7],[11800,"ZA-URU1","Peñarol","Fénix",1]
+let PARTIDOS_CON_CUOTA = [
+  [11830, 'Oriente Petrolero', 'CA Palmaflor Vinto', '0.5', 1.45],
+  [11600, 'Monagas SC', 'Academia Puerto Cabello', '0.5', 1.42],
+  [11815, 'Zamora FC', 'Deportivo Tachira', '0.5', 1.53],
+  [11700, 'Gimnasia LP', 'Godoy Cruz', 'Gimnasia LP', 2.25],
+  [11930, 'Banfield', 'Patronato Parana', 'Banfield', 2.00],
+  [11930, 'Sarmiento', 'Atletico Lanus', 'Atletico Lanus', 2.35],
+  [11810, 'Deportivo Moron', 'Club Almagro', 'Club Almagro', 2.87],
+  [11700, 'Operario PR', 'Nautico PE', 'Operario PR', 2.20],
+  [11700, 'Vila Nova FC', 'Ituano SP', 'Ituano SP', 2.55],
+  [11930, 'Guarani SP', 'Gremio RS', 'Gremio RS', 2.25],
+  [11745, 'Independiente Medellin', 'La Equidad', 'Independiente Medellin', 2.05],
+  [11900, 'Tecnico Universitario', 'Cumbaya FC', 'Cumbaya FC', 3.30],
+  [11700, 'Mineros de Zacatecas', 'Atlante', 'Atlante', 2.15],
+  [11815, 'Club General Caballero JLM', 'Club Tacuary', 'Club General Caballero JLM', 2.05],
+  [11600, 'Resistencia', 'Guairena FC', 'Guairena FC', 2.10],
+  [11800, 'Nacional Montevideo', 'Rentistas', 'Nacional Montevideo', 1.48]
 ]
 
-let PARTIDOS_CON_CUOTA = [
-  [11800, 'Atletico Bucaramanga', 'Tolima', 'Tolima', 2.10],
-  [11500, 'Fortaleza FC', 'Bogota FC', 'Fortaleza FC', 1.70],
-  [11600, 'Deportes Quindio', 'Valledupar', 'Deportes Quindio', 1.80],
-  [11900, 'Club Nueve de Octubre', 'Orense SC', 'Club Nueve de Octubre', 2.05],
-  [11345, 'Sochaux', 'Paris FC', 'Paris FC', 2.45],
-  [11300, 'Sport Boys Association', 'Universidad Cesar Vallejo', 'Sport Boys Association', 2.30],
-  [11300, 'Liverpool Montevideo', 'Defensor Sporting', 'Liverpool Montevideo', 2.00],
-  [11800, 'Penarol Montevideo', 'Atletico Fenix Montevideo', 'Penarol Montevideo', 1.85],
-  [11400, 'Watford', 'Sheffield United', 'Watford', 2.10],
-  [11230, 'AFC Hermanstadt', 'FC Botosani', 'AFC Hermanstadt', 2.30],
-  [11200, 'Korona Kielce', 'Slask Wroclaw', 'Slask Wroclaw', 1.85],
-  [11700, 'Defensa Y Justicia', 'Arsenal de Sarandi', 'Defensa Y Justicia', 1.95],
-  [11700, 'Rosario Central', 'Central Cordoba', 'Rosario Central', 1.93],
-  [11930, 'Colon de Santa Fe', 'CA Independiente', 'Colon de Santa Fe', 2.10],
-  [11300, 'Club Almagro', 'Temperley', 'Club Almagro', 2.20],
-  [11300, 'Guillermo Brown', 'Santamarina Tandil', 'Guillermo Brown', 1.90],
-  [11300, 'Nueva Chicago', 'CSD Flandria', 'Nueva Chicago', 2.15],
-  [11300, 'Tristan Suarez', 'CA San Telmo', 'Tristan Suarez', 1.93],
-  [11400, 'Mitre Santiago Del Estero', 'Deportivo Riestra', 'Mitre Santiago Del Estero', 2.20],
-  [11700, 'Club Atletico Alvarado', 'Deportivo Moron', 'Club Atletico Alvarado', 2.75],
-  [11900, 'San Martin de Tucuman', 'Chaco For Ever', 'San Martin de Tucuman', 2.05],
-  [11735, 'Ferro Carril Oeste', 'CA Belgrano', 'CA Belgrano', 2.62],
-  [11940, 'Instituto AC Cordoba', 'CA All Boys', 'CA All Boys', 3.10],
-  [11800, 'Santos SP', 'Fluminense RJ', 'Santos SP', 2.35],
-  [12005, 'Atlético Nacional', 'Deportivo Pasto', 'Atlético Nacional', 1.85]
+let PARTIDOS_SIN_CUOTA = [
+  [11600,"*ZA-VEN1","Monagas","Puerto Cabello",11],[11600,"ZA-PAR1","Resistencia","Guairena FC",8],[11700,"ZA-ARG1","Gimnasia L.P.","Godoy Cruz",9],[11700,"ZA-BRA2","Operario","Náutico",7],[11700,"ZA-BRA2","Vila Nova","Ituano",10],[11700,"ZA-MEX2","Mineros","Atlante",8],[11745,"ZA-COL1","Ind. Medellín","La Equidad",5],[11800,"ZA-URU1","Nacional","Rentistas",5],[11810,"ZA-ARG2","Deportivo Morón","Almagro",10],[11815,"*ZA-VEN1","Zamora","Dep. Tá",11],[11815,"ZA-PAR1","General Caballero JLM","Tacuary",7],[11830,"*ZA-BOL1","Oriente Petrolero","Palmaflor",11],[11900,"ZA-ECU1","Técnico U.","Cumbaya",10],[11930,"ZA-ARG1","Banfield","Patronato",7],[11930,"ZA-ARG1","Sarmiento","Lanús",10],[11930,"ZA-BRA2","Guaraní","Gremio",2],[11950,"ZA-COL1","Patriotas","Cortuluá",7],[12105,"ZA-MEX1","Mazatlán","Guadalajara",8],[12130,"ZA-EST1","Vancouver Whitecaps","Houston Dynamo",7]
 ]
 
 let PARTIDOS_CON_CUOTA_Y_GRUPO = [];
