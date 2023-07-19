49d36cbf7b944ccde6c4d30261591f9201684f2a
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2023-07-19 15:00:22 -0500

    Add files via upload

--- logica_partidos_wplay.js
+++ logica_partidos_wplay.js
@@ -1,88 +1,109 @@
+let PARTIDOS_BACKUP = [...PARTIDOS_OPTIMIZADOS]
 
 const CONTENEDOR_DATOS = document.getElementById('contenedor-datos')
 const CONTENEDOR_DATOS_SM = document.getElementById('contenedor-datos-sm')
 
-const dibujar = function ( partidos = PARTIDOS_OPTIMIZADOS) {
+const dibujar = function (partidos = PARTIDOS_OPTIMIZADOS) {
     let html = ''
     let indice = 0
     let total = partidos.length
     for (const partido of partidos) {
         indice++
-        let partido_string = `${ partido.local } - ${ partido.visitante }`
+        let partido_string = `${partido.local} - ${partido.visitante}`
         // let cantidad_de_letras = partido_string.length
         html += `
-        <div class="row ${ partido.cantidadDeApuestas < 43 ? 'bg-warning': ''}">
+        <div class="row ${partido.cantidadDeApuestas < 43 ? 'bg-warning' : ''}">
         <div class="col-2 border text-end">
-            <strong>${ indice }/${ total } - ${ partido.hora - 10000 }</strong>
+            <strong>${indice}/${total} - ${partido.hora - 10000}</strong>
         </div>
         <div class="col-1 border">
-            <strong>${ partido.liga }</strong>
+            <strong>${partido.liga}</strong>
         </div>
         <div class="col-3 border">
-            <strong>${ partido_string.substring(0,30) }</strong>
+            <strong>${partido_string.substring(0, 30)}</strong>
         </div>
         <div class="col border">
             <div class="row">
-                <div class="col-4 border">${ partido.cuotaLocal }</div>
-                <div class="col-4 border">${ partido.empate }</div>
-                <div class="col-4 border">${ partido.cuotaVisitante }</div>
+                <div class="col-4 border">${partido.cuotaLocal}</div>
+                <div class="col-4 border">${partido.empate}</div>
+                <div class="col-4 border">${partido.cuotaVisitante}</div>
             </div>
         </div>
         <div class="col border">
-            <strong>${ partido.cantidadDeApuestas }</strong>
+            <strong>${partido.cantidadDeApuestas}</strong>
         </div> 
         <div class="col border">
-            <strong>${ partido.cuotaCualquiera || '' }</strong>
+            <strong>${partido.cuotaCualquiera || ''}</strong>
         </div> 
     </div>
         `
     }
-    
+
     CONTENEDOR_DATOS.innerHTML = html
 }
 
-const dibujarSM = function ( partidos = PARTIDOS_OPTIMIZADOS) {
+const dibujarSM = function (partidos = PARTIDOS_OPTIMIZADOS) {
     let html = ''
     let indice = 0
     let total = partidos.length
     for (const partido of partidos) {
         indice++
         html += `
-        <div class="row ${ partido.cantidadDeApuestas < 43 ? 'bg-warning': ''}">
+        <div class="row ${partido.cantidadDeApuestas < 43 ? 'bg-warning' : ''}">
             <div class="col-2 border">
-                ${ indice }/${ total }<br>
-                ${ partido.hora - 10000 }
+                ${indice}/${total}<br>
+                ${partido.hora - 10000}
             </div>
             <div class="col-6 border">
-                ${ partido.liga }
+                ${partido.liga}
                 <br>
-                <small>${ partido.local.substring(0,15) }</small>
+                <small>${partido.local.substring(0, 15)}</small>
                 <br>
-                <small>${ partido.visitante.substring(0,15) }</small>
+                <small>${partido.visitante.substring(0, 15)}</small>
             </div>
             <div class="col-2 border">
-                ${ partido.cuotaLocal.toFixed(2) }
+                ${partido.cuotaLocal.toFixed(2)}
                 <br>
-                ${ partido.empate.toFixed(2) }
+                ${partido.empate.toFixed(2)}
                 <br>
-                ${ partido.cuotaVisitante.toFixed(2) }
+                ${partido.cuotaVisitante.toFixed(2)}
             </div>
             <div class="col-2 border">
-                ${ partido.cuotaCualquiera?.toFixed(2) || '' }
+                ${partido.cuotaCualquiera?.toFixed(2) || ''}
             </div>
         </div>
         `
     }
-    
+
     CONTENEDOR_DATOS_SM.innerHTML = html
 }
 
 dibujar()
 dibujarSM()
 
-const ordenar = function ( value ) {
+const ordenar = function (value) {
     console.log({ value })
-    PARTIDOS_OPTIMIZADOS.sort((a, b) => a[ value ] - b[ value ])
+    if (value === 'cuotaCualquiera') {
+        const filtrados = PARTIDOS_OPTIMIZADOS.filter(partido => partido.hasOwnProperty('cuotaCualquiera'))
+        PARTIDOS_OPTIMIZADOS = filtrados
+    }
+    if (value !== 'liga') {
+        PARTIDOS_OPTIMIZADOS.sort((a, b) => a[value] - b[value])
+    } else {
+        PARTIDOS_OPTIMIZADOS.sort((a, b) => {
+            const nameA = a[value].toUpperCase(); // ignore upper and lowercase
+            const nameB = b[value].toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+
+            // names must be equal
+            return 0;
+        });
+    }
     dibujar()
     dibujarSM()
 }
