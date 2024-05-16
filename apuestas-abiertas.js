2724905de6c817e8d27ebf7f4d503b3a2b840b31
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-16 17:00:00 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,7 @@
 let APUESTAS_ABIERTAS = [
+	[[11700,"Fluminense RJ","Cerro Porteño","local",1.85],[11700,"Universitario de Deportes","Botafogo RJ","visitante",2.3],"42,550"],
+	[[11700,"Fluminense RJ","Cerro Porteño","over",2.3],[11700,"Universitario de Deportes","Botafogo RJ","over",2.3],"52,900"],
+	[[11700,"Fluminense RJ","Cerro Porteño","mitad",1.48],[11700,"Universitario de Deportes","Botafogo RJ","mitad",1.5],"22,200"],
 	[[11900,"San Lorenzo","Liverpool Montevideo","local",1.85],[11700,"Fluminense RJ","Cerro Porteño","local",1.85],[11700,"Universitario de Deportes","Botafogo RJ","visitante",2.3],[11900,"Sportivo Luqueno","Bragantino SP","visitante",1.769],"139,269"],
 	[[11700,"Fluminense RJ","Cerro Porteño","over",2.3],[11700,"Universitario de Deportes","Botafogo RJ","over",2.3],[11900,"San Lorenzo","Liverpool Montevideo","over",2.45],[11900,"Sao Paulo SP","Barcelona SC","over",1.9],[11700,"Rosario Central","Caracas FC","over",1.75],[11900,"Sportivo Luqueno","Bragantino SP","over",2],[11900,"Independiente Juniors","Manta FC","over",2.3],"1,982,308"],
 	[[11700,"Fluminense RJ","Cerro Porteño","mitad",1.48],[11700,"Universitario de Deportes","Botafogo RJ","mitad",1.5],[11900,"San Lorenzo","Liverpool Montevideo","mitad",1.5],[11900,"Sportivo Luqueno","Bragantino SP","mitad",1.4],[11900,"Independiente Juniors","Manta FC","mitad",1.45],"67,599"],
