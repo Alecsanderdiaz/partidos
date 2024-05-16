bbaf285876d428440f4cb8ba39b859c00c8bb5b9
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-16 16:15:39 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,16 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[
-		[11510, "Deportivo Riestra", "Newells Old Boys", "visitante", 1.85],
-		"171,414"
-	],
-	[
-		[11415, "FC Penafiel", "Torreense", "over", 1.95],
-		[11500, "Real Sociedad", "Valencia", "over", 2.2],
-		[11510, "Deportivo Riestra", "Newells Old Boys", "over", 3],
-		"231,660"
-	],
-	[
-		[11510, "Deportivo Riestra", "Newells Old Boys", "mitad", 1.615],
-		"52,319"
-	],
+	[[11900,"San Lorenzo","Liverpool Montevideo","local",1.85],[11700,"Fluminense RJ","Cerro Porteño","local",1.85],[11700,"Universitario de Deportes","Botafogo RJ","visitante",2.3],[11900,"Sportivo Luqueno","Bragantino SP","visitante",1.769],"139,269"],
+	[[11700,"Fluminense RJ","Cerro Porteño","over",2.3],[11700,"Universitario de Deportes","Botafogo RJ","over",2.3],[11900,"San Lorenzo","Liverpool Montevideo","over",2.45],[11900,"Sao Paulo SP","Barcelona SC","over",1.9],[11700,"Rosario Central","Caracas FC","over",1.75],[11900,"Sportivo Luqueno","Bragantino SP","over",2],[11900,"Independiente Juniors","Manta FC","over",2.3],"1,982,308"],
+	[[11700,"Fluminense RJ","Cerro Porteño","mitad",1.48],[11700,"Universitario de Deportes","Botafogo RJ","mitad",1.5],[11900,"San Lorenzo","Liverpool Montevideo","mitad",1.5],[11900,"Sportivo Luqueno","Bragantino SP","mitad",1.4],[11900,"Independiente Juniors","Manta FC","mitad",1.45],"67,599"],
 ]
