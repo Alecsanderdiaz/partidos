43b7c3c8e149d52c7e7e59c4313c0720872e218e
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-16 10:05:41 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,26 +1,7 @@
 let APUESTAS_ABIERTAS = [
-	[
-		[11230, "Las Palmas", "Real Betis", "visitante", 1.95],
-		[11000, "FC Zlin", "SK Dynamo Ceske Budejovice", "over", 1.8],
-		[11000, "Bohemians 1905", "MFK Karvina", "over", 1.8],
-		[11000, "FK Baumit Jablonec", "FK Pardubice", "over", 1.769],
-		[11050, "FC Smorgon", "Isloch", "over", 1.833],
-		[11200, "Corum Belediyespor", "Kocaelispor", "over", 1.833],
-		[11245, "Neman Grodno", "FK Arsenal Dzerzhinsk", "over", 1.95],
-		"732,625"
-	],
-	[
-		[10900, "Bodrum Belediyesi Bodrumspor", "Boluspor", "over", 2.2],
-		[11230, "Las Palmas", "Real Betis", "over", 2.1],
-		[11000, "Bohemians 1905", "MFK Karvina", "local", 1.75],
-		[11000, "FK Baumit Jablonec", "FK Pardubice", "local", 1.769],
-		[11000, "FC Zlin", "SK Dynamo Ceske Budejovice", "visitante", 2.2],
-		"314,693"
-	],
-	[
-		[11230, "Las Palmas", "Real Betis", "mitad", 1.444],
-		[10900, "Bodrum Belediyesi Bodrumspor", "Boluspor", "mitad", 1.48],
-		[11245, "Neman Grodno", "FK Arsenal Dzerzhinsk", "mitad", 1.4],
-		"29,929"
-	],
+	[[11000,"FC Zlin","SK Dynamo Ceske Budejovice","mitad",1.363],[11000,"Bohemians 1905","MFK Karvina","mitad",1.363],[11000,"FK Baumit Jablonec","FK Pardubice","mitad",1.363],[11050,"FC Smorgon","Isloch","mitad",1.333],"33,809"],
+	[[11000,"Bohemians 1905","MFK Karvina","local",1.769],[11000,"FK Baumit Jablonec","FK Pardubice","local",1.727],[11000,"FC Zlin","SK Dynamo Ceske Budejovice","visitante",2.2],"67,231"],
+	[[11000,"FC Zlin","SK Dynamo Ceske Budejovice","over",1.8],[11000,"FK Baumit Jablonec","FK Pardubice","over",1.8],[11000,"Bohemians 1905","MFK Karvina","over",1.8],[11050,"FC Smorgon","Isloch","over",1.833],"106,920"],
+	[[11230,"Las Palmas","Real Betis","visitante",1.95],[11000,"FC Zlin","SK Dynamo Ceske Budejovice","over",1.8],[11000,"Bohemians 1905","MFK Karvina","over",1.8],[11000,"FK Baumit Jablonec","FK Pardubice","over",1.769],[11050,"FC Smorgon","Isloch","over",1.833],[11200,"Corum Belediyespor","Kocaelispor","over",1.833],[11245,"Neman Grodno","FK Arsenal Dzerzhinsk","over",1.95],"732,625"],
+	[[10900,"Bodrum Belediyesi Bodrumspor","Boluspor","over",2.2],[11230,"Las Palmas","Real Betis","over",2.1],[11000,"Bohemians 1905","MFK Karvina","local",1.75],[11000,"FK Baumit Jablonec","FK Pardubice","local",1.769],[11000,"FC Zlin","SK Dynamo Ceske Budejovice","visitante",2.2],"314,693"],
 ]
