af8188cfd3971c2d1ed10cadedce4b6bea6ab374
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-17 12:39:26 -0500

    Update apuestas-abiertas.js

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,10 +1,81 @@
 let APUESTAS_ABIERTAS = [
-	[[11345,"Troyes","Annecy FC","visitante",1.75],[11400,"Southampton","West Bromwich","visitante",2.65],[11345,"St Truiden","OH Leuven","visitante",1.9],[11300,"Roda JC","NAC Breda","visitante",2.2],[11345,"Raith Rovers","Partick Thistle","local",1.769],"342,961"],
-	[[11400,"Southampton","West Bromwich","over",1.8],[11345,"Raith Rovers","Partick Thistle","over",1.769],[11230,"Bate Borisov","Slavia Mozyr","over",1.833],[11345,"Grenoble","SC Amiens","local",1.909],[11345,"Fiorentina","Napoles","local",1.85],[11330,"Palermo","Sampdoria","local",2.05],[11330,"Mirandes","Elche","visitante",1.93],"815,846"],
-	[[11330,"Mirandes","Elche","over",2.15],[11330,"Palermo","Sampdoria","over",2.25],[11330,"Gimnasia y Tiro de Salta","Temperley","over",3],[11345,"SC Bastia","Paris FC","over",1.75],[11345,"EA Guingamp","Laval","over",1.8],[11345,"Grenoble","SC Amiens","over",1.833],"838,097"],
-	[[11330,"Mirandes","Elche","mitad",1.45],[11330,"Palermo","Sampdoria","mitad",1.5],[11345,"Fiorentina","Napoles","mitad",1.333],[11345,"Angers SCO","Dunkerque","mitad",1.285],[11345,"SC Bastia","Paris FC","mitad",1.333],[11345,"EA Guingamp","Laval","mitad",1.35],[11345,"Grenoble","SC Amiens","mitad",1.363],[11345,"Rodez Aveyron","Ajaccio","mitad",1.333],[11345,"Troyes","Annecy FC","mitad",1.285],[11400,"Southampton","West Bromwich","mitad",1.35],[11345,"St Truiden","OH Leuven","mitad",1.285],[11330,"Gornik Zabrze","Puszcza Niepolomice","mitad",1.333],[11245,"FC Famalicao","Casa Pia Atletico","mitad",1.333],"484,119"],
-	[[11200,"Pacos Ferreira","Cf Os Belenenses","local",1.75],[11200,"Antalyaspor","Adana Demirspor","visitante",1.95],[11200,"Benfica B","FC Porto B","visitante",1.85],"63,131"],
-	[[11200,"FC Botosani","CS Mioveni","over",null],[11200,"FC Botosani","CS Mioveni","mitad",null],[11930,"Novorizontino SP","Coritiba PR","visitante",2.55],[11700,"CRB Maceio AL","Vila Nova FC","visitante",2.85],[11200,"Antalyaspor","Adana Demirspor","visitante",1.8],[11345,"Raith Rovers","Partick Thistle","local",1.769],[11445,"Rio Ave","Benfica","visitante",1.4],"32,402"],
-	[[11330,"Mirandes","Elche","over",2.15],[11330,"Palermo","Sampdoria","over",2.25],[11200,"FC Botosani","CS Mioveni","over",2.05],[11330,"Gimnasia y Tiro de Salta","Temperley","over",3],[11430,"Atlético Grau","Cusco FC","over",2],"595,013"],
-	[[11200,"Benfica B","FC Porto B","mitad",1.285],[11200,"Pacos Ferreira","Cf Os Belenenses","mitad",1.333],[11200,"FC Botosani","CS Mioveni","mitad",1.42],[11230,"Bate Borisov","Slavia Mozyr","mitad",1.4],[11315,"AC Bellinzona","FC Sion","mitad",1.3],[11315,"FC Aarau","FC Wil","mitad",1.285],[11330,"Gimnasia y Tiro de Salta","Temperley","mitad",1.7],[11345,"Raith Rovers","Partick Thistle","mitad",1.363],[11430,"Atlético Grau","Cusco FC","mitad",1.42],"187,509"],
+	[
+		[11200, "FC Botosani", "CS Mioveni", "over", 2],
+		"20,000"
+	],
+	[
+		[11200, "FC Botosani", "CS Mioveni", "mitad", 1.42],
+		"14,200"
+	],
+	[
+		[11200, "Pacos Ferreira", "Cf Os Belenenses", "local", 1.75],
+		[11200, "Antalyaspor", "Adana Demirspor", "visitante", 1.95],
+		[11200, "Benfica B", "FC Porto B", "visitante", 1.85],
+		"63,131"
+	],
+	[
+		[11330, "Mirandes", "Elche", "over", 2.15],
+		[11330, "Palermo", "Sampdoria", "over", 2.25],
+		[11200, "FC Botosani", "CS Mioveni", "over", 2.05],
+		[11330, "Gimnasia y Tiro de Salta", "Temperley", "over", 3],
+		[11430, "Atlético Grau", "Cusco FC", "over", 2],
+		"595,013"
+	],
+	[
+		[11200, "Pacos Ferreira", "Cf Os Belenenses", "mitad", 1.333],
+		[11200, "FC Botosani", "CS Mioveni", "mitad", 1.42],
+		[11230, "Bate Borisov", "Slavia Mozyr", "mitad", 1.4],
+		[11315, "AC Bellinzona", "FC Sion", "mitad", 1.3],
+		[11315, "FC Aarau", "FC Wil", "mitad", 1.285],
+		[11330, "Gimnasia y Tiro de Salta", "Temperley", "mitad", 1.7],
+		[11345, "Raith Rovers", "Partick Thistle", "mitad", 1.363],
+		[11430, "Atlético Grau", "Cusco FC", "mitad", 1.42],
+		"187,509"
+	],
+	[
+		[11400, "Southampton", "West Bromwich", "over", 1.8],
+		[11345, "Raith Rovers", "Partick Thistle", "over", 1.769],
+		[11230, "Bate Borisov", "Slavia Mozyr", "over", 1.833],
+		[11345, "Grenoble", "SC Amiens", "local", 1.909],
+		[11345, "Fiorentina", "Napoles", "local", 1.85],
+		[11330, "Palermo", "Sampdoria", "local", 2.05],
+		[11330, "Mirandes", "Elche", "visitante", 1.93],
+		"815,846"
+	],
+	[
+		[11330, "Mirandes", "Elche", "mitad", 1.45],
+		[11330, "Palermo", "Sampdoria", "mitad", 1.5],
+		[11345, "Fiorentina", "Napoles", "mitad", 1.333],
+		[11345, "Angers SCO", "Dunkerque", "mitad", 1.285],
+		[11345, "SC Bastia", "Paris FC", "mitad", 1.333],
+		[11345, "EA Guingamp", "Laval", "mitad", 1.35],
+		[11345, "Grenoble", "SC Amiens", "mitad", 1.363],
+		[11345, "Rodez Aveyron", "Ajaccio", "mitad", 1.333],
+		[11345, "Troyes", "Annecy FC", "mitad", 1.285],
+		[11400, "Southampton", "West Bromwich", "mitad", 1.35],
+		[11345, "St Truiden", "OH Leuven", "mitad", 1.285],
+		[11330, "Gornik Zabrze", "Puszcza Niepolomice", "mitad", 1.333],
+		[11245, "FC Famalicao", "Casa Pia Atletico", "mitad", 1.333],
+		"484,119"
+	],
+	[
+		[11345, "Troyes", "Annecy FC", "visitante", 1.75],
+		[11400, "Southampton", "West Bromwich", "visitante", 2.65],
+		[11345, "St Truiden", "OH Leuven", "visitante", 1.9],
+		[11300, "Roda JC", "NAC Breda", "visitante", 2.2],
+		[11345, "Raith Rovers", "Partick Thistle", "local", 1.769],
+		"342,961"
+	],
+
+	[
+		[11330, "Mirandes", "Elche", "over", 2.15],
+		[11330, "Palermo", "Sampdoria", "over", 2.25],
+		[11330, "Gimnasia y Tiro de Salta", "Temperley", "over", 3],
+		[11345, "SC Bastia", "Paris FC", "over", 1.75],
+		[11345, "EA Guingamp", "Laval", "over", 1.8],
+		[11345, "Grenoble", "SC Amiens", "over", 1.833],
+		"838,097"
+	],
+
+
 ]
