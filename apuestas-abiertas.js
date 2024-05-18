95f8910d95a1967d078e1f89c33de80032138f42
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-18 09:33:34 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,9 @@
 let APUESTAS_ABIERTAS = [
+	[[11030,"Slask Wroclaw","Radomiak Radom","local",1.769],[11030,"Atletico Fenix Montevideo","Progreso","visitante",2.2],"38,923"],
+	[[11000,"FC Minsk","FC Vitebsk","over",2.625],[11015,"Bolton Wanderers","Oxford United","over",1.8],[11030,"Slask Wroclaw","Radomiak Radom","over",1.95],[11030,"Atletico Fenix Montevideo","Progreso","over",2.15],"198,096"],
+	[[11000,"FC Minsk","FC Vitebsk","mitad",1.6],[11030,"Atletico Fenix Montevideo","Progreso","mitad",1.45],"23,200"],
 	[[10915,"SD Huesca","Racing Santander","local",2],[10915,"CD Leganes","Sporting Gijon","local",1.95],[10930,"Sporting Farense","Portimonense SC","local",1.85],[10930,"Arouca","Vitoria Guimaraes","visitante",1.85],[10930,"FC Dordrecht","FC Emmen","visitante",2.1],"280,303"],
 	[[10915,"SD Huesca","Racing Santander","over",2.15],[10915,"CD Leganes","Sporting Gijon","over",2.45],[10930,"Boavista","FC Vizela","over",1.833],[10930,"CF Estrela","Gil Vicente","over",1.7],"164,170"],
 	[[10915,"SD Huesca","Racing Santander","mitad",1.45],[10915,"CD Leganes","Sporting Gijon","mitad",1.533],"22,233"],
-	[[10830,"1. FC Heidenheim","Cologne","visitante",1.85],[10830,"Werder Bremen","VfL Bochum","visitante",1.95],[10830,"Wolfsburg","Mainz 05","visitante",1.769],[10830,"Eintracht Frankfurt","RB Leipzig","local",1.727],[10830,"Hoffenheim","Bayern Munich","local",1.769],[10830,"Union Berlin","Freiburg","local",1.727],"336,897"],
 	[[10800,"Rentistas","Cerrito CS","over",2.5],[10830,"Union Berlin","Freiburg","over",1.727],[10830,"Lokomotiv Moscow","Fakel Voronezh","over",2.1],"90,682"],
-	[[10800,"Rentistas","Cerrito CS","mitad",1.533],[10830,"Lokomotiv Moscow","Fakel Voronezh","mitad",1.45],"22,233"],
 ]
