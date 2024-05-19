12d9f9216ef42d50bec4821ce58ea5c9bd5c877f
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-19 10:55:53 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,32 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[
-		[11715, "La Equidad", "Tolima", "visitante", 2.4],
-		"24,000"
-	],
-	[
-		[11630, "Goias GO", "Botafogo SP", "over", 2.3],
-		[11630, "Club Tacuary", "Libertad Asuncion", "over", 1.85],
-		[11630, "Universidad de Chile", "Universidad Catolica", "over", 1.833],
-		[11715, "La Equidad", "Tolima", "over", 2.25],
-		[11700, "Quilmes AC", "Gimnasia Jujuy", "over", 2.6], "456,349"
-	],
-	[
-		[11630, "Goias GO", "Botafogo SP", "mitad", 1.48],
-		[11715, "La Equidad", "Tolima", "mitad", 1.48],
-		[11700, "Quilmes AC", "Gimnasia Jujuy", "mitad", 1.6], "35,046"
-	],
-	[
-		[11500, "Llaneros (F)", "Cúcuta Deportivo (F)", "over", 2.5],
-		[11530, "Amazonas FC", "Paysandu PA", "over", 2.45],
-		[11530, "Club Sport Emelec", "Macara", "over", 2.2],
-		[11500, "Deportivo La Guaira", "Inter de Barinas", "over", 2.1],
-		[11600, "Defensor Sporting", "Penarol Montevideo", "over", 2.05], "580,099"
-	],
-	[
-		[11530, "Amazonas FC", "Paysandu PA", "mitad", 1.55],
-		[11530, "Club Sport Emelec", "Macara", "mitad", 1.45],
-		[11500, "Deportivo La Guaira", "Inter de Barinas", "mitad", 1.444],
-		[11500, "Llaneros (F)", "Cúcuta Deportivo (F)", "mitad", 1.533],
-		[11600, "Defensor Sporting", "Penarol Montevideo", "mitad", 1.444], "71,902"
-	],
+	[[11200,"Real Betis","Real Sociedad","over",2.3],[11200,"CF Cadiz","Las Palmas","over",2.25],[11030,"Danubio","Racing Club Montevideo","over",2.2],[11130,"Nublense","OHiggins","over",2.05],[11145,"Club Atletico Atlanta","Atletico Rafaela","over",2.55],"278,531"],
+	[[11200,"CF Cadiz","Las Palmas","mitad",1.48],[11200,"Real Betis","Real Sociedad","mitad",1.5],[11130,"Nublense","OHiggins","mitad",1.42],[11145,"Club Atletico Atlanta","Atletico Rafaela","mitad",1.571],[11030,"Danubio","Racing Club Montevideo","mitad",1.5],"74,307"],
 ]
