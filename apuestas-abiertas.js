6c4a0990d28f56591641c3d0dbca4709f70e8a19
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-25 12:07:42 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,26 +1,23 @@
 let APUESTAS_ABIERTAS = [
 	[
+		[10100, "Argentinos Jrs", "River Plate", "mitad", 1.40],
+		[10100, "CA Agropecuario", "Club Atletico Defensores Unidos", "mitad", 1.50],
+		[10100, "Club Atletico Guemes", "Estudiantes De Río Cuarto", "mitad", 1.64],
+		"57,253"
+	],
+	[
+		[11300, "Argentinos Juniors", "River Plate", "mitad", 1.45],
+		[11230, "Panathinaikos", "Aris Thessaloniki", "over", 2.05],
+		[11300, "Agropecuario Argentino", "CA Defensores Unidos", "over", 2.5],
+		[11400, "Universitario de Vinto", "Club Aurora", "over", 2.2],
+		"159,073"
+	],
+	[
 		[11030, "Puszcza Niepolomice", "GKS Piast Gliwice", "over", 2.25],
 		[11030, "Miramar Misiones", "Montevideo Wanderers", "over", 2.15],
 		[11130, "Rayo Vallecano", "Athletic Bilbao", "over", 2],
 		[11045, "CA San Telmo", "Colon de Santa Fe", "over", 2.5],
 		[11130, "Tristan Suarez", "San Martin de San Juan", "over", 2.875],
 		"695,391"
-	],
-	[
-		[11030, "Puszcza Niepolomice", "GKS Piast Gliwice", "mitad", 1.5],
-		[11130, "Rayo Vallecano", "Athletic Bilbao", "mitad", 1.42],
-		"21,300"
-	],
-	[
-		[10915, "Real Sociedad", "Atlético Madrid", "over", 1.85],
-		"18,500"
-	],
-	[
-		[10900, "Manchester City", "Manchester United", "mitad", 1.222],
-		[10900, "Celtic", "Glasgow Rangers", "mitad", 1.3],
-		[10915, "Real Sociedad", "Atlético Madrid", "mitad", 1.38],
-		[10930, "FC Emmen", "NAC Breda", "mitad", 1.3],
-		"28,505"
 	],
 ]
