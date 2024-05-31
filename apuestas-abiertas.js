335cc985f3564ced109374b0637dee96beabc76f
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-05-31 15:53:07 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,47 @@
 let APUESTAS_ABIERTAS = [
-	[[11330,"FC Thun","Grasshoppers","local",1.909],[11100,"FK Mlada Boleslav","FC Hradec Kralove","visitante",2.1],"32,073"],
-	[[11130,"Bate Borisov","Slutsksakhar Slutsk","over",2.05],[11500,"Cucuta Deportivo","Llaneros FC","over",2.15],[11200,"La Luz FC","Cerrito CS","over",2.8],[11300,"Arsenal de Sarandi","Racing de Cordoba","over",2.8],"345,548"],
-	[[11330,"FC Thun","Grasshoppers","mitad",1.35],[11100,"FK Mlada Boleslav","FC Hradec Kralove","mitad",1.363],[11130,"Bate Borisov","Slutsksakhar Slutsk","mitad",1.444],[11200,"La Luz FC","Cerrito CS","mitad",1.571],[11300,"Arsenal de Sarandi","Racing de Cordoba","mitad",1.615],[11500,"Cucuta Deportivo","Llaneros FC","mitad",1.42],"85,850"],
+	[
+		[11700, "Ceara CE", "Coritiba PR", "visitante", 2.4],
+		"24,000"
+	],
+	[
+		[11700, "Ceara CE", "Coritiba PR", "over", 2.35],
+		[11700, "CA Atenas De San Carlos", "Albion FC", "over", 2.45],
+		"57,575"
+	],
+	[
+		[11700, "Ceara CE", "Coritiba PR", "mitad", 1.5],
+		[11700, "CA Atenas De San Carlos", "Albion FC", "mitad", 1.5],
+		"22,500"
+	],
+	[
+		[11930, "Goias GO", "Sport Recife PE", "local", 2],
+		[11930, "Cobreloa", "OHiggins", "local", 2.05],
+		[12100, "Costa Rica", "Uruguay", "visitante", 2.35],
+		[11700, "Ceara CE", "Coritiba PR", "visitante", 2.4],
+		[11900, "Tecnico Universitario", "El Nacional", "visitante", 2.35],
+		"543,414"
+	],
+	[
+		[11700, "Ceara CE", "Coritiba PR", "over", 2.3],
+		[11900, "Tecnico Universitario", "El Nacional", "over", 2.2],
+		[11930, "Goias GO", "Sport Recife PE", "over", 2.25],
+		[12100, "Costa Rica", "Uruguay", "over", 2.05],
+		[11500, "Cucuta Deportivo", "Llaneros FC", "over", 2.45],
+		[11700, "CA Atenas De San Carlos", "Albion FC", "over", 2.45],
+		[11910, "Colon de Santa Fe", "Atletico Rafaela", "over", 2.2],
+		"3,082,065"
+	],
+	[
+		[11700, "Ceara CE", "Coritiba PR", "mitad", 1.5],
+		[11900, "Tecnico Universitario", "El Nacional", "mitad", 1.48],
+		[11930, "Goias GO", "Sport Recife PE", "mitad", 1.48],
+		[12030, "Mexico", "Bolivia", "mitad", 1.333],
+		[12100, "Costa Rica", "Uruguay", "mitad", 1.42],
+		[11830, "New York City", "San Jose Earthquakes", "mitad", 1.25],
+		[11700, "CA Atenas De San Carlos", "Albion FC", "mitad", 1.5],
+		[11900, "Union Magdalena", "Deportes Quindio", "mitad", 1.4],
+		[11910, "Colon de Santa Fe", "Atletico Rafaela", "mitad", 1.45],
+		[11930, "Cobreloa", "OHiggins", "mitad", 1.4],
+		"331,487"
+	],
 ]
