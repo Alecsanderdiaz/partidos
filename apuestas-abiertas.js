a19289364d49f4756478a6296b528a67aab0f2b1
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-01 16:27:47 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,6 +1,7 @@
 let APUESTAS_ABIERTAS = [
 	[[11630,"Cuiaba MT","Internacional RS","visitante",2.35],[11630,"CS 2 de Mayo","Olimpia Asuncion","visitante",2.05],"48,175"],
+	[[11630,"Cuiaba MT","Internacional RS","over",2.8],[11630,"CS 2 de Mayo","Olimpia Asuncion","over",2.05],[11700,"Atletico Huila","Real Cartagena","over",2.3],[11710,"Talleres de Remedios de Escalada","CD Maipu","over",2.8],[11630,"Fluminense RJ","Juventude RS","over",1.85],"683,864"],
+	[[11630,"Cuiaba MT","Internacional RS","mitad",1.65],[11630,"CS 2 de Mayo","Olimpia Asuncion","mitad",1.444],[11700,"Atletico Huila","Real Cartagena","mitad",1.48],[11710,"Talleres de Remedios de Escalada","CD Maipu","mitad",1.65],"58,201"],
+	[[11630,"Cuiaba MT","Internacional RS","visitante",2.35],[11630,"CS 2 de Mayo","Olimpia Asuncion","visitante",2.05],"48,175"],
 	[[11500,"Brusque FC","Novorizontino SP","over",2.55],[11630,"Cuiaba MT","Internacional RS","over",2.8],[11700,"Atletico Huila","Real Cartagena","over",2.3],[11630,"CS 2 de Mayo","Olimpia Asuncion","over",2.05],[11710,"Talleres de Remedios de Escalada","CD Maipu","over",2.75],"925,790"],
-	[[11500,"Brusque FC","Novorizontino SP","mitad",1.571],[11630,"Cuiaba MT","Internacional RS","mitad",1.65],[11630,"CS 2 de Mayo","Olimpia Asuncion","mitad",1.444],[11700,"Atletico Huila","Real Cartagena","mitad",1.48],[11710,"Talleres de Remedios de Escalada","CD Maipu","mitad",1.65],"91,459"],
-	[[11400,"Gremio RS","Bragantino SP","over",2.15],[11400,"EC Vitoria","Atletico GO","over",2.3],[11400,"Club Tacuary","Nacional Asuncion","over",2.05],"101,373"],
 ]
