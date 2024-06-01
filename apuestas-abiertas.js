1a8c26351233c5194941bb039c50bd62c4b7ec1d
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-01 13:25:28 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11400,"Gremio RS","Bragantino SP","over",2.15],[11400,"EC Vitoria","Atletico GO","over",2.3],[11400,"Club Tacuary","Nacional Asuncion","over",2.05],"101,373"],
 	[[11530,"Montevideo Wanderers","Rampla Juniors","local",2.05],[11400,"Gremio RS","Bragantino SP","local",2.3],[11400,"Sportivo Trinidense","Guarani Asuncion","local",2.1],[11400,"EC Vitoria","Atletico GO","visitante",2.625],[11400,"Club Tacuary","Nacional Asuncion","visitante",2.2],"571,812"],
-	[[11000,"Arsenal Tula","FC Nizhny Novgorod","over",2.1],[11000,"FK Arsenal Dzerzhinsk","Dinamo Minsk","over",2.1],[11400,"Gremio RS","Bragantino SP","over",2.1],[11400,"EC Vitoria","Atletico GO","over",2.3],[11205,"Nueva Chicago","Temperley","over",3],[11330,"Chacarita Juniors","CA Guemes","over",2.75],[11400,"Club Tacuary","Nacional Asuncion","over",2.1],"3,690,277"],
-	[[11400,"EC Vitoria","Atletico GO","mitad",1.5],[11400,"Gremio RS","Bragantino SP","mitad",1.45],[11000,"Arsenal Tula","FC Nizhny Novgorod","mitad",1.45],[11000,"FK Arsenal Dzerzhinsk","Dinamo Minsk","mitad",1.45],[11205,"Nueva Chicago","Temperley","mitad",1.7],[11330,"Chacarita Juniors","CA Guemes","mitad",1.615],[11400,"Club Tacuary","Nacional Asuncion","mitad",1.45],"182,091"],
+	[[11400,"EC Vitoria","Atletico GO","mitad",1.5],[11400,"Gremio RS","Bragantino SP","mitad",1.45],[11330,"Chacarita Juniors","CA Guemes","mitad",1.615],[11400,"Club Tacuary","Nacional Asuncion","mitad",1.45],"182,091"],
 ]
