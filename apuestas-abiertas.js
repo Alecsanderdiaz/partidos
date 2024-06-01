a1ded01f384e71d8834f3c79f72548261221a5e5
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-01 15:01:55 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,6 @@
 let APUESTAS_ABIERTAS = [
+	[[11630,"Cuiaba MT","Internacional RS","visitante",2.35],[11630,"CS 2 de Mayo","Olimpia Asuncion","visitante",2.05],"48,175"],
+	[[11500,"Brusque FC","Novorizontino SP","over",2.55],[11630,"Cuiaba MT","Internacional RS","over",2.8],[11700,"Atletico Huila","Real Cartagena","over",2.3],[11630,"CS 2 de Mayo","Olimpia Asuncion","over",2.05],[11710,"Talleres de Remedios de Escalada","CD Maipu","over",2.75],"925,790"],
+	[[11500,"Brusque FC","Novorizontino SP","mitad",1.571],[11630,"Cuiaba MT","Internacional RS","mitad",1.65],[11630,"CS 2 de Mayo","Olimpia Asuncion","mitad",1.444],[11700,"Atletico Huila","Real Cartagena","mitad",1.48],[11710,"Talleres de Remedios de Escalada","CD Maipu","mitad",1.65],"91,459"],
 	[[11400,"Gremio RS","Bragantino SP","over",2.15],[11400,"EC Vitoria","Atletico GO","over",2.3],[11400,"Club Tacuary","Nacional Asuncion","over",2.05],"101,373"],
-	[[11530,"Montevideo Wanderers","Rampla Juniors","local",2.05],[11400,"Gremio RS","Bragantino SP","local",2.3],[11400,"Sportivo Trinidense","Guarani Asuncion","local",2.1],[11400,"EC Vitoria","Atletico GO","visitante",2.625],[11400,"Club Tacuary","Nacional Asuncion","visitante",2.2],"571,812"],
-	[[11400,"EC Vitoria","Atletico GO","mitad",1.5],[11400,"Gremio RS","Bragantino SP","mitad",1.45],[11330,"Chacarita Juniors","CA Guemes","mitad",1.615],[11400,"Club Tacuary","Nacional Asuncion","mitad",1.45],"182,091"],
 ]
