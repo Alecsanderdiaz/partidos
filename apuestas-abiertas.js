61bcb3df4601a18ea70cefe2cc35ce88711d2851
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-01 21:37:25 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11630,"Cuiaba MT","Internacional RS","visitante",2.35],[11630,"CS 2 de Mayo","Olimpia Asuncion","visitante",2.05],"48,175"],
-	[[11630,"Cuiaba MT","Internacional RS","over",2.8],[11630,"CS 2 de Mayo","Olimpia Asuncion","over",2.05],[11700,"Atletico Huila","Real Cartagena","over",2.3],[11710,"Talleres de Remedios de Escalada","CD Maipu","over",2.8],[11630,"Fluminense RJ","Juventude RS","over",1.85],"683,864"],
-	[[11630,"Cuiaba MT","Internacional RS","mitad",1.65],[11630,"CS 2 de Mayo","Olimpia Asuncion","mitad",1.444],[11700,"Atletico Huila","Real Cartagena","mitad",1.48],[11710,"Talleres de Remedios de Escalada","CD Maipu","mitad",1.65],"58,201"],
-	[[11630,"Cuiaba MT","Internacional RS","visitante",2.35],[11630,"CS 2 de Mayo","Olimpia Asuncion","visitante",2.05],"48,175"],
-	[[11500,"Brusque FC","Novorizontino SP","over",2.55],[11630,"Cuiaba MT","Internacional RS","over",2.8],[11700,"Atletico Huila","Real Cartagena","over",2.3],[11630,"CS 2 de Mayo","Olimpia Asuncion","over",2.05],[11710,"Talleres de Remedios de Escalada","CD Maipu","over",2.75],"925,790"],
+	[[12305,"Tokyo Verdy","Consadole Sapporo","over",1.85],[10300,"Kawasaki Frontale","Nagoya Grampus","over",1.85],[10400,"Indonesia","Tanzania","over",1.93],[10500,"FC Seoul","Gwangju FC","over",1.95],"128,806"],
+	[[10230,"Jeju United","Gangwon FC","over",2],[10230,"Daejeon Citizen","Daegu FC","over",2.1],[10500,"Kashiwa Reysol","Avispa Fukuoka","over",2.45],"102,900"],
+	[[10230,"Jeju United","Gangwon FC","mitad",1.42],[10230,"Daejeon Citizen","Daegu FC","mitad",1.444],[10500,"Kashiwa Reysol","Avispa Fukuoka","mitad",1.533],"31,450"],
+	[[12100,"Orange County Blues","Detroit City","over",2.15],[12130,"Portland Timbers","Houston Dynamo","local",1.85],[12130,"Vancouver Whitecaps FC","Colorado Rapids","visitante",2.15],"163,258"],
 ]
