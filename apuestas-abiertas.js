a84a8ae6c604f89ed1dd299f6a97dd66e1180e69
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-07 16:25:41 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,4 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Madagascar","Comores","over",2.85],[11100,"Armenia","Kazajistán","over",2.1],[11100,"Uganda","Botsuana","over",2.6],[11100,"Angola","Eswatini","over",2.1],[11200,"Albania","Azerbaiyán","over",2.2],[11200,"Bielorrusia","Rusia","over",1.95],"1,401,890"],
-	[[11200,"Albania","Azerbaiyán","mitad",1.48],[11200,"Bielorrusia","Rusia","mitad",1.4],[11100,"Armenia","Kazajistán","mitad",1.45],[11100,"Angola","Eswatini","mitad",1.45],[11100,"Uganda","Botsuana","mitad",1.571],[11100,"Madagascar","Comores","mitad",1.65],"112,955"],
+	[[11700,"Coritiba PR","Ituano SP","over",2.15],[11800,"San Lorenzo","Chacarita Juniors","over",3.1],[11900,"Novorizontino SP","Santos SP","over",2.55],[11930,"Perú","Paraguay","over",2.7],"458,885"],
+	[[11700,"Coritiba PR","Ituano SP","mitad",1.45],[11800,"San Lorenzo","Chacarita Juniors","mitad",1.6],[11900,"Novorizontino SP","Santos SP","mitad",1.571],[11930,"Perú","Paraguay","mitad",1.6],[11600,"America de Cali (F)","Atletico Nacional (F)","mitad",1.4],"81,664"],
 ]
