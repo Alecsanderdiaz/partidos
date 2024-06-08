a07b8a6285adea236fb543e9be52e9f709438070
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-08 08:56:38 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11700,"Coritiba PR","Ituano SP","over",2.15],[11800,"San Lorenzo","Chacarita Juniors","over",3.1],[11900,"Novorizontino SP","Santos SP","over",2.55],[11930,"Perú","Paraguay","over",2.7],"458,885"],
-	[[11700,"Coritiba PR","Ituano SP","mitad",1.45],[11800,"San Lorenzo","Chacarita Juniors","mitad",1.6],[11900,"Novorizontino SP","Santos SP","mitad",1.571],[11930,"Perú","Paraguay","mitad",1.6],[11600,"America de Cali (F)","Atletico Nacional (F)","mitad",1.4],"81,664"],
+	[[11100,"Estonia","Islas Feroe","local",2.05],[11100,"Moldavia","Chipre","local",2.05],[11100,"Suiza","Austria","local",1.95],[11145,"Portugal","Croacia","local",1.75],[11030,"Liverpool Montevideo","Miramar Misiones","local",1.833],[11100,"Suecia","Serbia","visitante",2],[11130,"Real Oviedo","SD Eibar","visitante",2.35],"1,235,719"],
+	[[11100,"Suiza","Austria","over",2.1],[11100,"Moldavia","Chipre","over",2.45],[11100,"Estonia","Islas Feroe","over",2.15],[11130,"Real Oviedo","SD Eibar","over",2.25],"248,889"],
+	[[11130,"Real Oviedo","SD Eibar","mitad",1.48],[11100,"Moldavia","Chipre","mitad",1.55],[11100,"Suiza","Austria","mitad",1.444],[11145,"Portugal","Croacia","mitad",1.38],[11100,"Suecia","Serbia","mitad",1.38],[11000,"CD Real San Andres (F)","Deportivo Pereira (F)","mitad",1.35],[11030,"Liverpool Montevideo","Miramar Misiones","mitad",1.4],"119,265"],
+	[[10800,"Eslovenia","Bulgaria","over",2.1],[10800,"Letonia","Lituania","over",2.05],"101,168"],
 ]
