d082c23b74259faf6fa9115a262cf0b368200165
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-10 10:31:09 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11100,"Uganda","Argelia","over",2.7],[11100,"Benin","Nigeria","over",2.6],[11100,"Guinea Bissau","Egipto","over",2.35],"164,970"],
+	[[11100,"Uganda","Argelia","mitad",1.571],[11100,"Benin","Nigeria","mitad",1.55],[11100,"Guinea Bissau","Egipto","mitad",1.5],"36,536"],
+	[[11400,"Ghana","Central African Rep.","over",2.1],[11400,"Guinea Sub-23","Mozambique","over",2.2],[11400,"Burkina Faso","Sierra Leona","over",2.35],[11500,"Deportivo Cali (F)","Millonarios (F)","over",2.45],[11500,"Deportivo Pasto (F)","Alianza FC (F)","over",2.45],[11500,"Internacional de Palmira (F)","Llaneros (F)","over",2.15],"1,401,137"],
 ]
