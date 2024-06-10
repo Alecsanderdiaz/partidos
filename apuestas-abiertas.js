6eb6e21fade0942110850f011dcd44069cf3d65c
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-10 10:21:41 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,4 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Argentina","Ecuador","over",2.1],[11800,"Club Leones Del Norte","Gualaceo SC","over",2.5],[11630,"America Mineiro MG","Ponte Preta SP","over",1.95],[11600,"CA Cerro","Nacional Montevideo","over",1.95],[11700,"CA Atenas De San Carlos","Montevideo City Torque","over",2.35],[11830,"La Luz FC","Plaza Colonia","over",2.4],"1,125,920"],
-	[[11630,"America Mineiro MG","Ponte Preta SP","mitad",1.4],[11800,"Argentina","Ecuador","mitad",1.42],[11600,"CA Cerro","Nacional Montevideo","mitad",1.4],[11700,"CA Atenas De San Carlos","Montevideo City Torque","mitad",1.48],[11800,"Club Leones Del Norte","Gualaceo SC","mitad",1.533],[11830,"La Luz FC","Plaza Colonia","mitad",1.5],"94,740"],
 ]
