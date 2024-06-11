82a5c70f3ab2085b831a23e123b8925b933a6de8
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-11 18:09:44 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,44 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[
-		[11100,"Bielorrusia","Israel","over",2.05],
-		[11200,"EAU","Barein","over",2.35],
-		[11200,"Irán","Uzbekistan Sub-23","over",2.15],
-		[11100,"Egypt U23","Ivory Coast U23","over",2],
-		"68,692"
-	],
-	[
-		[11000,"Turkmenistan","Hong Kong","over",2.25],
-		[11100,"Chad","Comores","over",2.45],
-		[11100,"Sudafrica","Zimbabue","over",2.1],
-		[11100,"Cabo Verde","Libya","over",2.375],
-		[11100,"Zambia","Tanzania","over",2.5],
-		[11100,"Libano","Bangladesh","over",2.15],
-		[11100,"Oman","Kirguistan","over",2.15],
-		"3,177,228"
-	],
-	[
-		[10800,"Kenia","Costa de Marfil","over",1.95],
-		[10800,"Madagascar","Mali","over",2.3],
-		[10800,"South Sudan","Sudán","over",2.95],
-		[10800,"Mauricio","Eswatini","over",3.1],
-		[10800,"Albion FC","Juventud de Las Piedras","over",2.5],
-		[10900,"Azerbaiyán","Kazajistán","over",2.25],
-		"2,307,112"
-	],
-	[
-		[11200,"EAU","Barein","mitad",1.5],
-		[10900,"Azerbaiyán","Kazajistán","mitad",1.48],
-		[11045,"Qatar","India","mitad",1.4],
-		[10800,"Madagascar","Mali","mitad",1.5],
-		[10800,"South Sudan","Sudán","mitad",1.7],
-		[10800,"Mauricio","Eswatini","mitad",1.65],
-		[10800,"Albion FC","Juventud de Las Piedras","mitad",1.533],
-		[11100,"Chad","Comores", "Total", 1.09],
-		[11100,"Lesotho","Ruanda","mitad",1.666],
-		[11100,"Sudafrica","Zimbabue","mitad",1.42],
-		[11100,"Cabo Verde","Libya","mitad",1.48],
-		[11100,"Zambia","Tanzania","mitad",1.55],
-		[11100,"Egypt U23","Ivory Coast U23","mitad",1.4],
-		"1,662,604"
-	],
+	[[11800,"Botafogo RJ","Fluminense RJ","local",1.909],[11930,"Bragantino SP","Atletico MG","local",2.2],[11900,"Chile","Paraguay","local",1.85],"50,505"],
+	[[11900,"Chile","Paraguay","over",2.25],[11900,"Guayaquil City FC","Vargas Torres","over",2.55],[11930,"Bragantino SP","Atletico MG","over",2.35],[11800,"Botafogo RJ","Fluminense RJ","over",2.15],"289,887"],
+	[[11800,"Botafogo RJ","Fluminense RJ","mitad",1.42],[11900,"Chile","Paraguay","mitad",1.5],[11930,"Bragantino SP","Atletico MG","mitad",1.5],[11900,"Guayaquil City FC","Vargas Torres","mitad",1.55],"49,523"],
+	[[11700,"Atletico GO","Corinthians SP","over",2.55],[11700,"Juventude RS","EC Vitoria","over",2.5],[11700,"Mirassol SP","Goias GO","over",2.55],[11800,"Botafogo RJ","Fluminense RJ","over",2.15],[11900,"Chile","Paraguay","over",2.25],[11930,"Bragantino SP","Atletico MG","over",2.25],"1,769,391"],
 ]
