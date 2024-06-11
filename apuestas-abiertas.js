a5718065346f4206c23ada445dcf64feb7e16d91
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-10 21:22:56 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,16 +1,44 @@
 let APUESTAS_ABIERTAS = [
 	[
-		[11930, "Sport Recife PE", "Paysandu PA", "local", 1.7],
-		"17,000"
+		[11100,"Bielorrusia","Israel","over",2.05],
+		[11200,"EAU","Barein","over",2.35],
+		[11200,"Irán","Uzbekistan Sub-23","over",2.15],
+		[11100,"Egypt U23","Ivory Coast U23","over",2],
+		"68,692"
 	],
 	[
-		[11910, "Atletico Rafaela", "Estudiantes Rio Cuarto", "over", 3],
-		[11915, "Atletico Huila", "Tigres FC", "over", 1.9],
-		[11930, "Sport Recife PE", "Paysandu PA", "over", 2.1],
-		"119,700"
+		[11000,"Turkmenistan","Hong Kong","over",2.25],
+		[11100,"Chad","Comores","over",2.45],
+		[11100,"Sudafrica","Zimbabue","over",2.1],
+		[11100,"Cabo Verde","Libya","over",2.375],
+		[11100,"Zambia","Tanzania","over",2.5],
+		[11100,"Libano","Bangladesh","over",2.15],
+		[11100,"Oman","Kirguistan","over",2.15],
+		"3,177,228"
 	],
 	[
-		[11930, "Sport Recife PE", "Paysandu PA", "mitad", 1.42],
-		"24,140"
+		[10800,"Kenia","Costa de Marfil","over",1.95],
+		[10800,"Madagascar","Mali","over",2.3],
+		[10800,"South Sudan","Sudán","over",2.95],
+		[10800,"Mauricio","Eswatini","over",3.1],
+		[10800,"Albion FC","Juventud de Las Piedras","over",2.5],
+		[10900,"Azerbaiyán","Kazajistán","over",2.25],
+		"2,307,112"
+	],
+	[
+		[11200,"EAU","Barein","mitad",1.5],
+		[10900,"Azerbaiyán","Kazajistán","mitad",1.48],
+		[11045,"Qatar","India","mitad",1.4],
+		[10800,"Madagascar","Mali","mitad",1.5],
+		[10800,"South Sudan","Sudán","mitad",1.7],
+		[10800,"Mauricio","Eswatini","mitad",1.65],
+		[10800,"Albion FC","Juventud de Las Piedras","mitad",1.533],
+		[11100,"Chad","Comores", "Total", 1.09],
+		[11100,"Lesotho","Ruanda","mitad",1.666],
+		[11100,"Sudafrica","Zimbabue","mitad",1.42],
+		[11100,"Cabo Verde","Libya","mitad",1.48],
+		[11100,"Zambia","Tanzania","mitad",1.55],
+		[11100,"Egypt U23","Ivory Coast U23","mitad",1.4],
+		"1,662,604"
 	],
 ]
