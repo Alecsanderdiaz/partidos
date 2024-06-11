fd7c7041d759893442d87410f0743ac8bf1f9518
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-10 19:57:29 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,17 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"Deportivo Cali (F)","Millonarios (F)","over",2.35],[11500,"Deportivo Pasto (F)","Alianza FC (F)","over",2.45],[11500,"Independiente Santa Fe (F)","America de Cali (F)","over",2.1],"120,908"],
-	[[11500,"Deportivo Cali (F)","Millonarios (F)","mitad",1.48],[11500,"Deportivo Pasto (F)","Alianza FC (F)","mitad",1.55],[11500,"Independiente Santa Fe (F)","America de Cali (F)","mitad",1.42],"32,575"],
-	[[11345,"Polonia","Turquia","over",1.95],[11400,"Ghana","Central African Rep.","over",2.1],[11400,"Guinea Sub-23","Mozambique","over",2.2],[11400,"Burkina Faso","Sierra Leona","over",2.5],"225,225"],
-	[[11400,"Guinea Sub-23","Mozambique","mitad",1.45],[11400,"Burkina Faso","Sierra Leona","mitad",1.55],"44,680"],
-	[[11400,"Ghana","Central African Rep.","over",2.1],[11400,"Guinea Sub-23","Mozambique","over",2.2],[11400,"Burkina Faso","Sierra Leona","over",2.35],[11500,"Deportivo Cali (F)","Millonarios (F)","over",2.45],[11500,"Deportivo Pasto (F)","Alianza FC (F)","over",2.45],[11500,"Internacional de Palmira (F)","Llaneros (F)","over",2.15],"1,401,137"],
+	[
+		[11930, "Sport Recife PE", "Paysandu PA", "local", 1.7],
+		"17,000"
+	],
+	[
+		[11910, "Atletico Rafaela", "Estudiantes Rio Cuarto", "over", 3],
+		[11915, "Atletico Huila", "Tigres FC", "over", 1.9],
+		[11930, "Sport Recife PE", "Paysandu PA", "over", 2.1],
+		"119,700"
+	],
+	[
+		[11930, "Sport Recife PE", "Paysandu PA", "mitad", 1.42],
+		[11910, "Atletico Rafaela", "Estudiantes Rio Cuarto", "mitad", 1.7],
+		"24,140"
+	],
 ]
