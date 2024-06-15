6a3a3290a1665b8ceb695f77fa365f2b22d9bf8d
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-15 12:41:45 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[10500,"Kyoto Sanga","Consadole Sapporo","local",1.85],[10500,"Cerezo Osaka","Urawa Red Diamonds","local",2.05],[10600,"Gangwon FC","Suwon City FC","local",1.769],[10200,"Yokohama F Marinos","Machida Zelvia","visitante",1.769],[10500,"Gwangju FC","Gimcheon Sangmu","visitante",2.2],[10700,"Henan Songshan Longmen","Beijing Guoan FC","visitante",1.9],[10800,"Hungria","Suiza","visitante",2.05],"1,017,243"],
-	[[10800,"Hungria","Suiza","over",2.25],[10800,"Cooper","Uruguay Montevideo FC","over",2.25],"50,625"],
-	[[10800,"Hungria","Suiza","mitad",1.48],[10800,"Cooper","Uruguay Montevideo FC","mitad",1.45],"21,460"],
+	[[11600,"Huracan","Independiente Rivadavia","local",1.9],[11300,"Atletico River Plate (Uru)","Deportivo Maldonado","local",2.1],[11400,"Union Santa Fe","San Lorenzo","visitante",3.35],[11600,"Ituano SP","Paysandu PA","visitante",2.375],"587,291"],
+	[[11300,"Atletico River Plate (Uru)","Deportivo Maldonado","over",2.15],[11330,"America Mineiro MG","CRB Maceio AL","over",2.1],[11400,"Italia","Albania","over",2],[11400,"Union Santa Fe","San Lorenzo","over",3.7],[11500,"Ponte Preta SP","Novorizontino SP","over",2.6],"1,824,241"],
+	[[11330,"America Mineiro MG","CRB Maceio AL","mitad",1.45],[11400,"Union Santa Fe","San Lorenzo","mitad",1.8],[11500,"Ponte Preta SP","Novorizontino SP","mitad",1.6],[11600,"Huracan","Independiente Rivadavia","mitad",1.55],[11600,"Ituano SP","Paysandu PA","mitad",1.55],"144,919"],
 ]
