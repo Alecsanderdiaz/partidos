54e0659dfd80792d9bdc3fa466f55684430c9f28
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-14 20:17:15 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[10700,"Nantong Zhiyun","Shenzhen XinPengcheng","local",2.2],"22,000"],
-	[[11145,"FC Vitebsk","Torpedo Zhodino","over",2],"20,000"],
-	[[11145,"FC Vitebsk","Torpedo Zhodino","mitad",1.42],"14,200"],
+	[[10500,"Kyoto Sanga","Consadole Sapporo","local",1.85],[10500,"Cerezo Osaka","Urawa Red Diamonds","local",2.05],[10600,"Gangwon FC","Suwon City FC","local",1.769],[10200,"Yokohama F Marinos","Machida Zelvia","visitante",1.769],[10500,"Gwangju FC","Gimcheon Sangmu","visitante",2.2],[10700,"Henan Songshan Longmen","Beijing Guoan FC","visitante",1.9],[10800,"Hungria","Suiza","visitante",2.05],"1,017,243"],
+	[[10800,"Hungria","Suiza","over",2.25],[10800,"Cooper","Uruguay Montevideo FC","over",2.25],"50,625"],
+	[[10800,"Hungria","Suiza","mitad",1.48],[10800,"Cooper","Uruguay Montevideo FC","mitad",1.45],"21,460"],
 ]
