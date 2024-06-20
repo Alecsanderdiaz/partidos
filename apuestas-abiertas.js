816e2dad4b74426738e2a4981c119861ff814af9
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-20 16:06:01 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Dinamarca","Inglaterra","over",2.2],[11400,"España","Italia","over",2.25],[11400,"Llaneros (F)","Millonarios (F)","over",2.45],[11500,"Independiente Juniors","Club Nueve de Octubre","over",2.55],"309,251"],
-	[[11400,"Nublense","Huachipato","local",2],[11400,"España","Italia","local",2.05],[11100,"Dinamarca","Inglaterra","visitante",1.8],"73,800"],
-	[[11100,"Dinamarca","Inglaterra","mitad",1.48],[11400,"España","Italia","mitad",1.48],[11400,"Llaneros (F)","Millonarios (F)","mitad",1.55],[11500,"Independiente Juniors","Club Nueve de Octubre","mitad",1.5],"50,927"],
+	[[11630,"EC Vitoria","Atletico MG","over",2.6],[11700,"Vila Nova FC","Mirassol SP","over",2.55],[11930,"Palmeiras SP","Bragantino SP","over",2],[11930,"Ceara CE","Sport Recife PE","over",2.1],"278,460"],
+	[[11630,"EC Vitoria","Atletico MG","mitad",1.6],[11700,"Vila Nova FC","Mirassol SP","mitad",1.571],[11930,"Palmeiras SP","Bragantino SP","mitad",1.42],[11930,"Ceara CE","Sport Recife PE","mitad",1.45],"51,769"],
+	[[11630,"EC Vitoria","Atletico MG","visitante",2.45],[11930,"Ceara CE","Sport Recife PE","visitante",2.3],[11700,"Vila Nova FC","Mirassol SP","visitante",2.35],"132,423"],
 ]
