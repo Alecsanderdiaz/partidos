2164799ba1ffaff7ce61451feebb0c654f57076b
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-24 13:53:38 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,4 @@
 let APUESTAS_ABIERTAS = [
+	[[11300,"Albion FC","Club Oriental de La Paz","over",2.25],[11400,"Croacia","Italia","over",2],[11610,"Club Atletico Atlanta","Almirante Brown","over",2.85],[11700,"Colombia","Paraguay","over",2.25],[11700,"Sport Recife PE","Novorizontino SP","over",2.2],"634,838"],
+	[[11400,"Croacia","Italia","mitad",1.42],[11700,"Colombia","Paraguay","mitad",1.48],[11700,"Sport Recife PE","Novorizontino SP","mitad",1.48],[11610,"Club Atletico Atlanta","Almirante Brown","mitad",1.65],"74,416"],
 ]
