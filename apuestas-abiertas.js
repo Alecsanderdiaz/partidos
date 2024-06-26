86f78455ac6688c176f1526d49567ec9d258be77
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-26 12:47:38 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,36 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Eslovaquia","Rumania","over",3.1],"31,000"],
-	[[11100,"Eslovaquia","Rumania","mitad",1.66],"16,667"],
-	[[11100,"Eslovaquia","Rumania","local",2.5],[11100,"FC St. Gallen","Ludogorets","local",1.9],"42,750"],
+	[
+		[11400,"Republica Checa","Turquia","visitante",2.15],
+		"21,500"
+	],
+	[
+		[11400,"Republica Checa","Turquia","over",2],
+		[11400,"Deportes Concepcion","Deportes Linares","over",2.15],
+		[11500,"Independiente Juniors","Club Leones Del Norte","over",2.3],
+		[11500,"Chacaritas SC","Gualaceo SC","over",2.6],
+		[11500,"Club Nueve de Octubre","Vargas Torres","over",2.65],
+		"681,421"
+	],
+	[
+		[11400,"Republica Checa","Turquia","mitad",1.42],
+		[11500,"Chacaritas SC","Gualaceo SC","mitad",1.55],
+		[11400,"Deportes Concepcion","Deportes Linares","mitad",1.42],
+		[11500,"Independiente Juniors","Club Leones Del Norte","mitad",1.45],
+		[11500,"Club Nueve de Octubre","Vargas Torres","mitad",1.55],
+		"70,244"
+	],
+	[
+		[11100,"Eslovaquia","Rumania","over",3.1],
+		"31,000"
+	],
+	[
+		[11400,"Deportes Concepcion","Deportes Linares","mitad",1.42],
+		[11500,"Independiente Juniors","Club Leones Del Norte","mitad",1.42],
+		[11500,"Chacaritas SC","Gualaceo SC","mitad",1.55],
+		[11500,"Club Nueve de Octubre","Vargas Torres","mitad",1.6],
+		[11800,"Detroit City","Birmingham Legion FC","mitad",1.42],
+		[11800,"CSD Rangers","Nublense","mitad",1.333],
+		[11830,"Rhode Island FC","El Paso Locomotive FC","mitad",1.35],
+		"115,087"
+	],
 ]
