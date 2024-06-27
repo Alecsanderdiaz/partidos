096d812675ca9f2ac828ee6cb9f9bcd2054822ef
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-27 17:08:37 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,9 +1,6 @@
 let APUESTAS_ABIERTAS = [
-	[[11500,"Cuniburo FC","Guayaquil City FC","over",2.4],"24,000"],
-	[[11500,"Cuniburo FC","Guayaquil City FC","mitad",1.53],"15,333"],
-	[[11300,"Juventud de Las Piedras","Colon FC de Uruguay","over",2.15],"21,500"],
-	[[11300,"Juventud de Las Piedras","Colon FC de Uruguay","mitad",1.42],"14,200"],
-	[[11700,"Fluminense RJ","EC Vitoria","mitad",1.42],[11500,"Cuniburo FC","Guayaquil City FC","mitad",1.5],"21,300"],
-	[[11300,"Juventud de Las Piedras","Colon FC de Uruguay","over",2.05],[11500,"Cuniburo FC","Guayaquil City FC","over",2.5],[11700,"Fluminense RJ","EC Vitoria","over",2.1],[11800,"Sao Paulo SP","Criciuma SC","over",2],"215,250"],
+	[[11700,"Fluminense RJ","EC Vitoria","local",1.95],[11800,"Sao Paulo SP","Criciuma SC","local",1.727],[12000,"Uruguay","Bolivia","local",1.42],"47,828"],
+	[[11700,"Fluminense RJ","EC Vitoria","over",2.15],[11800,"Sao Paulo SP","Criciuma SC","over",2.05],[11900,"Magallanes Santiago","Curico Unido","over",2.1],"92,558"],
+	[[11700,"Fluminense RJ","EC Vitoria","mitad",1.5],[11800,"Sao Paulo SP","Criciuma SC","mitad",1.4],[12000,"Uruguay","Bolivia","mitad",1.3],"27,300"],
 	[[11700,"Fluminense RJ","EC Vitoria","local",1.76],"17,692"],
 ]
