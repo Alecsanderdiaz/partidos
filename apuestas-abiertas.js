e34c63ec98b14fb5f77882b09c045cbcc29597c5
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-06-28 19:04:31 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11700,"Colombia","Costa Rica","local",1.5],[12000,"Paraguay","Brasil","visitante",1.533],"23,000"],
-	[[11700,"Colombia","Costa Rica","over",1.8],[11830,"Charleston Battery","North Carolina","over",1.95],[11930,"Memphis 901 FC","Phoenix Rising FC","over",1.769],[12000,"Paraguay","Brasil","over",1.833],"113,850"],
-	[[11700,"Colombia","Costa Rica","mitad",1.333],[11830,"New York City","Orlando City SC","mitad",1.25],[12000,"Paraguay","Brasil","mitad",1.333],[11600,"San Antonio Unido","Universidad de Chile","mitad",1.25],[11830,"Charleston Battery","North Carolina","mitad",1.4],[11930,"Deportes Iquique","Antofagasta","mitad",1.285],[11930,"Cobreloa","San Marcos De Arica","mitad",1.333],[11930,"Memphis 901 FC","Phoenix Rising FC","mitad",1.35],"90,000"],
+	[[12000,"Paraguay","Brasil","mitad",1.333],[11830,"Charleston Battery","North Carolina","mitad",1.4],[11930,"Deportes Iquique","Antofagasta","mitad",1.285],[11930,"Cobreloa","San Marcos De Arica","mitad",1.35],[11930,"Memphis 901 FC","Phoenix Rising FC","mitad",1.35],"54,675"],
+	[[12000,"Paraguay","Brasil","visitante",1.533],"23,000"],
+	[[11830,"Charleston Battery","North Carolina","over",1.95],[11930,"Memphis 901 FC","Phoenix Rising FC","over",1.769],[12000,"Paraguay","Brasil","over",1.833],"113,850"],
 ]
