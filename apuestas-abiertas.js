6fd51106b55505eea045a17d3a85a0182f75e7fd
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-07-04 21:27:04 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,7 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11800,"Corinthians SP","EC Vitoria","local",1.85],[11800,"Fluminense RJ","Internacional RS","visitante",2.4],"44,400"],
-	[[11800,"Fluminense RJ","Internacional RS","over",2.5],[11800,"Corinthians SP","EC Vitoria","over",2.25],"56,250"],
-	[[11800,"Fluminense RJ","Internacional RS","mitad",1.55],[11800,"Corinthians SP","EC Vitoria","mitad",1.48],"22,940"],
-	[[11700,"Gremio RS","Palmeiras SP","over",2.25],[11705,"Deportivo Cali (F)","America de Cali (F)","over",2.15],"48,375"],
-	[[11700,"Bahia BA","Juventude RS","over",2.1],[11700,"Gremio RS","Palmeiras SP","over",2.1],[11800,"Fluminense RJ","Internacional RS","over",2.45],[11800,"Corinthians SP","EC Vitoria","over",2.25],[12000,"Argentina","Ecuador","over",2.375],[11705,"Deportivo Cali (F)","America de Cali (F)","over",2.15],"3,475,740"],
+	[[10530,"Incheon United","Gimcheon Sangmu","mitad",1.444],[11100,"España","Alemania","mitad",1.444],[11400,"Portugal","Francia","mitad",1.5],[11300,"Plaza Colonia","Albion FC","mitad",1.48],[11430,"Rentistas","La Luz FC","mitad",1.5],[11300,"Torpedo Zhodino","Slavia Mozyr","mitad",1.6],"111,164"],
+	[[11400,"Portugal","Francia","local",2.5],[10500,"Sanfrecce Hiroshima","Vissel Kobe","visitante",2.1],[10530,"Incheon United","Gimcheon Sangmu","visitante",2.15],[10630,"Viktoria Plzen","Karlsruher SC","visitante",2.2],[10800,"FC Basel","Eintracht Braunschweig","visitante",2.25],[11100,"España","Alemania","visitante",2.2],"1,229,209"],
+	[[10530,"Incheon United","Gimcheon Sangmu","over",2],[11100,"España","Alemania","over",2.1],[11300,"Plaza Colonia","Albion FC","over",2.35],[11400,"Portugal","Francia","over",2.35],[11430,"Rentistas","La Luz FC","over",2.5],[11300,"Torpedo Zhodino","Slavia Mozyr","over",2.65],"1,536,636"],
 ]
