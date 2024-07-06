ab78ad27eb361072cfd67c777907e02201ee942d
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-07-06 17:31:48 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,8 +1,5 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Inglaterra","Suiza","local",2.1],[11030,"CA Boston River","CA Cerro","visitante",2.5],"52,500"],
-	[[11100,"Inglaterra","Suiza","over",2.55],[11100,"FC Vitebsk","Isloch","over",2.25],[11030,"CA Boston River","CA Cerro","over",2.25],"129,094"],
-	[[11100,"Inglaterra","Suiza","mitad",1.55],[11030,"CA Boston River","CA Cerro","mitad",1.5],[11100,"FC Vitebsk","Isloch","mitad",1.5],"34,875"],
-	[[10800,"Cerro Largo FC","Liverpool Montevideo","over",2.15],[10800,"Cooper","Institucion Atletica Sud America","over",2.3],[10900,"America Mineiro MG","Operario PR","over",2.25],"111,263"],
-	[[10900,"America Mineiro MG","Operario PR","local",1.75],[10800,"Cerro Largo FC","Liverpool Montevideo","visitante",2.05],[10800,"Young Boys","Westerlo","visitante",2.4],[10700,"Cangzhou Mighty Lions","Shenzhen XinPengcheng","visitante",2.05],[10700,"Lech Poznan","Dundee","visitante",2.2],"388,311"],
-	[[10900,"America Mineiro MG","Operario PR","mitad",1.48],[10800,"Cooper","Institucion Atletica Sud America","mitad",1.48],[10800,"Cerro Largo FC","Liverpool Montevideo","mitad",1.48],"32,418"],
+	[[11800,"Sao Paulo SP","Bragantino SP","mitad",1.5],[12000,"Uruguay","Brasil","mitad",1.615],[11800,"Detroit City","Tampa Bay Rowdies","mitad",1.42],"34,408"],
+	[[12000,"Chivas Guadalajara","Toluca","local",1.93],[11800,"Sao Paulo SP","Bragantino SP","local",2.05],[11830,"CF Montreal","Vancouver Whitecaps FC","visitante",1.9],[11830,"Philadelphia Union","New York Red Bulls","visitante",1.85],[11930,"Austin FC","New York City","visitante",1.909],[12000,"Uruguay","Brasil","visitante",2.4],[11830,"FC Cincinnati","Inter Miami CF","visitante",2.2],"1,401,835"],
+	[[11800,"Flamengo RJ","Cuiaba MT","over",2],[11800,"Sao Paulo SP","Bragantino SP","over",2.3],[11800,"Detroit City","Tampa Bay Rowdies","over",2],[11800,"Pittsburgh Riverhounds","Monterey Bay FC","over",2],[12000,"Uruguay","Brasil","over",2.7],"496,800"],
 ]
