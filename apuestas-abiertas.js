2d9c765be895503af9f7fae96fe8b01329a11219
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-07-06 07:11:15 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,8 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"España","Alemania","over",2.1],[11300,"Plaza Colonia","Albion FC","over",2.55],[11300,"Torpedo Zhodino","Slavia Mozyr","over",2.7],[11400,"Portugal","Francia","over",2.375],[11430,"Rentistas","La Luz FC","over",2.55],"875,643"],
-	[[10800,"FC Basel","Eintracht Braunschweig","visitante",2.05],[11100,"España","Alemania","visitante",2.2],[11400,"Portugal","Francia","local",2.55],"115,005"],
-	[[11100,"España","Alemania","mitad",1.444],[11400,"Portugal","Francia","mitad",1.5],[11300,"Plaza Colonia","Albion FC","mitad",1.48],[11430,"Rentistas","La Luz FC","mitad",1.5],[11300,"Torpedo Zhodino","Slavia Mozyr","mitad",1.6],"111,164"],
+	[[11100,"Inglaterra","Suiza","local",2.1],[11030,"CA Boston River","CA Cerro","visitante",2.5],"52,500"],
+	[[11100,"Inglaterra","Suiza","over",2.55],[11100,"FC Vitebsk","Isloch","over",2.25],[11030,"CA Boston River","CA Cerro","over",2.25],"129,094"],
+	[[11100,"Inglaterra","Suiza","mitad",1.55],[11030,"CA Boston River","CA Cerro","mitad",1.5],[11100,"FC Vitebsk","Isloch","mitad",1.5],"34,875"],
+	[[10800,"Cerro Largo FC","Liverpool Montevideo","over",2.15],[10800,"Cooper","Institucion Atletica Sud America","over",2.3],[10900,"America Mineiro MG","Operario PR","over",2.25],"111,263"],
+	[[10900,"America Mineiro MG","Operario PR","local",1.75],[10800,"Cerro Largo FC","Liverpool Montevideo","visitante",2.05],[10800,"Young Boys","Westerlo","visitante",2.4],[10700,"Cangzhou Mighty Lions","Shenzhen XinPengcheng","visitante",2.05],[10700,"Lech Poznan","Dundee","visitante",2.2],"388,311"],
+	[[10900,"America Mineiro MG","Operario PR","mitad",1.48],[10800,"Cooper","Institucion Atletica Sud America","mitad",1.48],[10800,"Cerro Largo FC","Liverpool Montevideo","mitad",1.48],"32,418"],
 ]
