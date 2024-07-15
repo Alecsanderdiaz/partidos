c805790116c81c56db80cb30b321f3c121a58828
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-07-15 13:12:25 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11330,"Petrolul Ploiesti","Gloria Buzau","over",2.4],[11430,"Cooper","Albion FC","over",2.4],[11700,"Rentistas","Colon FC de Uruguay","over",2.625],[12015,"Boyaca Chico FC","Once Caldas","over",2.35],"355,320"],
+	[[12015,"Boyaca Chico FC","Once Caldas","mitad",1.5],[11330,"Petrolul Ploiesti","Gloria Buzau","mitad",1.533],[11430,"Cooper","Albion FC","mitad",1.5],[11700,"Rentistas","Colon FC de Uruguay","mitad",1.55],"53,475"],
+	[[11330,"Petrolul Ploiesti","Gloria Buzau","local",2.1],[12015,"Boyaca Chico FC","Once Caldas","visitante",2.375],"49,875"],
 ]
