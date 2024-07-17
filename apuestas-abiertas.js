37ab7f4f01474d8cf4c5e94e6b875f6193e40df6
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-07-17 07:46:47 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[11100,"Lincoln Red Imps","Hamrun Spartans","local",2.2],[11345,"Ayr United","Falkirk","local",1.85],[11345,"Cove","Spartans FC","local",1.75],[11345,"Elgin City","Kelty Hearts FC","local",2.15],[11400,"Shamrock Rovers","Vikingur Reykjavik","local",2.1],[11100,"HJK Helsinki","FK Panevezys","visitante",2.875],[11100,"FC Pyunik Yerevan","Dinamo Minsk","visitante",2.25],"2,080,226"],
-	[[11100,"Lincoln Red Imps","Hamrun Spartans","over",2.1],[11100,"FC Pyunik Yerevan","Dinamo Minsk","over",2.15],[11400,"Shamrock Rovers","Vikingur Reykjavik","over",2.1],"94,815"],
-	[[11100,"Lincoln Red Imps","Hamrun Spartans","mitad",1.45],[11400,"Shamrock Rovers","Vikingur Reykjavik","mitad",1.444],"20,944"],
 ]
