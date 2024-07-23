dad868c965147e597d8fdf22a86939aa79c62ace
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-07-22 23:07:56 -0500

    Add files via upload

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,2 +1,5 @@
 let APUESTAS_ABIERTAS = [
+	[[11100,"KF Ballkani","Hamrun Spartans","local",1.95],[11230,"FCSB","Maccabi Tel Aviv","local",1.95],[11345,"Spartans FC","Forfar Athletic","local",1.769],[11200,"Differdange FC 03","Ordabasy Shymkent","visitante",2.15],[11300,"Deportivo Riestra","Argentinos Juniors","visitante",2.4],[11300,"Dynamo Kiev","Partizan Belgrade","visitante",2.6],[11430,"Sporting De Lisboa","Sevilla","visitante",1.909],"1,723,072"],
+	[[11100,"KF Ballkani","Hamrun Spartans","over",2.4],[11200,"Differdange FC 03","Ordabasy Shymkent","over",2.05],[11300,"Deportivo Riestra","Argentinos Juniors","over",3.3],[11345,"Spartans FC","Forfar Athletic","over",2.1],[11500,"San Antonio","Cuniburo FC","over",2.25],"767,151"],
+	[[11300,"Deportivo Riestra","Argentinos Juniors","mitad",1.727],[11345,"Spartans FC","Forfar Athletic","mitad",1.444],[11100,"KF Ballkani","Hamrun Spartans","mitad",1.533],[11200,"Differdange FC 03","Ordabasy Shymkent","mitad",1.42],[11500,"San Antonio","Cuniburo FC","mitad",1.45],"78,769"],
 ]
