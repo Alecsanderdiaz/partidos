06cbb0654c1713afb490870e5c739aae00cfcb35
John Alexander Diaz <Alecsanderdiaz@users.noreply.github.com>
2024-08-23 17:42:16 -0500

    Update apuestas-abiertas.js

--- apuestas-abiertas.js
+++ apuestas-abiertas.js
@@ -1,5 +1,2 @@
 let APUESTAS_ABIERTAS = [
-	[[10230,"CD Leganes","Alcorcon","local",1.769],[10500,"Kashima Antlers","Brighton","local",2.2],[10800,"Argentina Olímpico","Marruecos Olímpico","local",1.9],[10930,"Wolfsburg","Lille","local",1.9],[11000,"Guinea Olímpico","Nueva Zelanda Olímpico","local",1.8],[11000,"ADO Den Haag","Zulte Waregem","local",1.769],[11030,"Frosinone","Bari","local",1.9],[10900,"Cologne","Swansea","visitante",2.15],[11000,"FC Struga Trim Lum","FC Pyunik Yerevan","visitante",2.3],"3,153,207"],
-	[[10800,"Argentina Olímpico","Marruecos Olímpico","over",2.1],[11000,"Guinea Olímpico","Nueva Zelanda Olímpico","over",2.15],[11000,"FC Struga Trim Lum","FC Pyunik Yerevan","over",2],"67,725"],
-	[[10800,"Argentina Olímpico","Marruecos Olímpico","mitad",1.42],[11000,"Guinea Olímpico","Nueva Zelanda Olímpico","mitad",1.45],[11000,"FC Struga Trim Lum","FC Pyunik Yerevan","mitad",1.42],"21,928"],
 ]
